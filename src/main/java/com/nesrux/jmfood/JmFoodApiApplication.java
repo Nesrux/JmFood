dbe39bca8f91dc97c4f6d2166916835942defa0c
Nesrux <joaomarcosdevs@gmail.com>
2023-03-07 22:38:04 -0300

    inicio do projeto

---
+++ src/main/java/com/nesrux/jmfood/JmFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.nesrux.jmfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nesrux/jmfood/JmFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesrux.jmfood;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
