81d57bdd936c2cad46297a875805df8fcd8e85d2
Nesrux <joaomarcosdevs@gmail.com>
2023-03-08 14:42:30 -0300

    Criando tableas automaticamente com o hibernate

--- src/main/java/com/nesrux/jmfood/domain/model/Cozinha.java
+++ src/main/java/com/nesrux/jmfood/domain/model/Cozinha.java
@@ -1,5 +1,7 @@
 package com.nesrux.jmfood.domain.model;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -26,6 +28,23 @@ public class Cozinha {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cozinha other = (Cozinha) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
