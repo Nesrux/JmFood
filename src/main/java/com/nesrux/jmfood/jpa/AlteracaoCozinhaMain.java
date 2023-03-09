6ea6e7b36c927c825f82d65e2c020572d3ff74aa
Nesrux <joaomarcosdevs@gmail.com>
2023-03-09 18:22:32 -0300

    Renomeando método de update

--- src/main/java/com/nesrux/jmfood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/nesrux/jmfood/jpa/AlteracaoCozinhaMain.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import com.nesrux.jmfood.JmFoodApiApplication;
 import com.nesrux.jmfood.domain.model.Cozinha;
 
-public class InclusãoCozinhaMain {
+public class AlteracaoCozinhaMain {
 	public static void main(String[] args) {
 		ApplicationContext app = new SpringApplicationBuilder(JmFoodApiApplication.class).web(WebApplicationType.NONE)
 				.run(args);
@@ -15,13 +15,9 @@ public class InclusãoCozinhaMain {
 		CadastroCozinha cadastroCozinha = app.getBean(CadastroCozinha.class);
 
 		Cozinha cozinha = new Cozinha("Paulista");
-		Cozinha cozinha2 = new Cozinha("Carioca");
-		Cozinha cozinha3 = new Cozinha("Mineira");
-		Cozinha cozinha4 = new Cozinha("Cearense");
+		cozinha.setId(1L);
 
 		cadastroCozinha.salvar(cozinha);
-		cadastroCozinha.salvar(cozinha2);
-		cadastroCozinha.salvar(cozinha3);
-		cadastroCozinha.salvar(cozinha4);
+		
 	}
 }
