0f33d29fa0f3a464a5b7d5b1b08e1a2273e66a8b
Nesrux <joaomarcosdevs@gmail.com>
2023-03-13 19:25:58 -0300

    Classe FormaPagamento

--- src/main/java/com/nesrux/jmfood/domain/model/FormaPagamento.java
+++ src/main/java/com/nesrux/jmfood/domain/model/FormaPagamento.java
@@ -1,5 +1,23 @@
 package com.nesrux.jmfood.domain.model;
 
-public class FormaPagamento {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class FormaPagamento {
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String descricao;
 }
