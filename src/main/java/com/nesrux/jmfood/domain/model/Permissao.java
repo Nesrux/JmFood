cb93cf877cadbb8e64effa175b8ff9760f95f4dd
Nesrux <joaomarcosdevs@gmail.com>
2023-03-13 19:29:33 -0300

    Classe Permissao

--- src/main/java/com/nesrux/jmfood/domain/model/Permissao.java
+++ src/main/java/com/nesrux/jmfood/domain/model/Permissao.java
@@ -1,5 +1,27 @@
 package com.nesrux.jmfood.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Permissao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String descricao;
 }
