8ad0bc4fb08d44baf739cdadf84516120de73a18
Nesrux <joaomarcosdevs@gmail.com>
2023-03-13 16:46:00 -0300

    editando o import.sql para colocar as FKS

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,14 @@
-insert into cozinha (nome) values('tailandesa')
-insert into cozinha (nome) values('Brasileira')
-insert into cozinha (nome) values('Japonesa')
-insert into cozinha (nome) values('Espanhola')
-insert into cozinha (nome) values('Portuguesa')
-insert into cozinha (nome) values('Romena')
-insert into cozinha (nome) values('teste')
+insert into cozinha (id, nome) values(1 ,'tailandesa')
+insert into cozinha (id, nome) values(2 , 'Brasileira')
+insert into cozinha (id, nome) values(3 , 'Japonesa')
+insert into cozinha (id, nome) values(4 ,'Espanhola')
+insert into cozinha (id, nome) values(5 , 'Portuguesa')
+insert into cozinha (id, nome) values(6, 'Romena')
+insert into cozinha (id, nome) values(7, 'teste')
 
 
-insert into restaurante (nome, taxa_frete) values  ('EAI BURGUER', 2.7) 
-insert into restaurante (nome, taxa_frete) values  ('Irmaos burguers', 1.7) 
-insert into restaurante (nome, taxa_frete) values ('Thai Gourmet', 10);
-insert into restaurante (nome, taxa_frete) values ('Thai Delivery', 9.50);
-insert into restaurante (nome, taxa_frete) values ('Tuk Tuk Comida Indiana', 15);
+insert into restaurante (nome, taxa_frete, cozinha_id) values  ('EAI BURGUER', 2.7, 2) 
+insert into restaurante (nome, taxa_frete, cozinha_id) values  ('Irmaos burguers', 1.7, 2) 
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 6)
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 5)
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('KOREWA nandesuka', 15, 3 )
