98e59439a8b328d8f5dc95435ea04948f6df2387
Nesrux <joaomarcosdevs@gmail.com>
2023-03-13 19:36:43 -0300

    Interface permissao

--- src/main/java/com/nesrux/jmfood/domain/repository/PermissaoRepository.java
+++ src/main/java/com/nesrux/jmfood/domain/repository/PermissaoRepository.java
@@ -1,5 +1,17 @@
 package com.nesrux.jmfood.domain.repository;
 
+import java.util.List;
+
+import com.nesrux.jmfood.domain.model.Permissao;
+import com.nesrux.jmfood.domain.model.Restaurante;
+
 public interface PermissaoRepository {
+	List<Permissao> listar();
+
+	Restaurante salvar(Restaurante restaurante);
+
+	void remover(Restaurante restaurante);
+
+	Restaurante buscar(Long id);
 
 }
