215dabb74fd1241494ab006e2b866561ab729676
Nesrux <joaomarcosdevs@gmail.com>
2023-03-13 15:40:59 -0300

    adicionando dados ao banco de dados /RESTAURANTE

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,10 @@ insert into cozinha (nome) values('Espanhola')
 insert into cozinha (nome) values('Portuguesa')
 insert into cozinha (nome) values('Romena')
 insert into cozinha (nome) values('teste')
+
+
+insert into restaurante (nome, taxa_frete) values  ('EAI BURGUER', 2.7) 
+insert into restaurante (nome, taxa_frete) values  ('Irmaos burguers', 1.7) 
+insert into restaurante (nome, taxa_frete) values ('Thai Gourmet', 10);
+insert into restaurante (nome, taxa_frete) values ('Thai Delivery', 9.50);
+insert into restaurante (nome, taxa_frete) values ('Tuk Tuk Comida Indiana', 15);
