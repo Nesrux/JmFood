7368eb36c7122e36a2443c9aebea95d0b8955db2
Nesrux <joaomarcosdevs@gmail.com>
2023-03-14 13:48:07 -0300

    implementação permissaoRepository

---
+++ src/main/java/com/nesrux/jmfood/infrastructure/repository/PermissaiRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.nesrux.jmfood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.nesrux.jmfood.domain.model.Permissao;
+import com.nesrux.jmfood.domain.repository.PermissaoRepository;
+
+public class PermissaiRepositoryImpl implements PermissaoRepository {
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public Permissao buscar(Long id) {
+		return manager.find(Permissao.class, id);
+	}
+
+	@Override
+	public Permissao salvar(Permissao permissao) {
+		return manager.merge(permissao);
+	}
+
+	@Override
+	public void remover(Permissao permissao) {
+		permissao = buscar(permissao.getId());
+		manager.remove(permissao);
+	}
+
+	@Override
+	public List<Permissao> listar() {
+		return manager.createQuery("from Permissao", Permissao.class).getResultList();
+	}
+
+
+}
