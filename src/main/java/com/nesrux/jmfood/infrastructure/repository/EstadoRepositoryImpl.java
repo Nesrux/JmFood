2791e3123c0491f5d077e094d56de1cc7524f41f
Nesrux <joaomarcosdevs@gmail.com>
2023-03-14 13:36:55 -0300

    implementacao EstadoRepository

---
+++ src/main/java/com/nesrux/jmfood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.nesrux.jmfood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.nesrux.jmfood.domain.model.Estado;
+import com.nesrux.jmfood.domain.repository.EstadoRepository;
+
+public class EstadoRepositoryImpl implements EstadoRepository {
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public Estado buscar(Long id) {
+
+		return manager.find(Estado.class, id);
+	}
+
+	@Override
+	public Estado salvar(Estado estado) {
+		return manager.merge(estado);
+	}
+
+	@Override
+	public void remover(Estado estado) {
+		estado = buscar(estado.getId());
+		manager.remove(estado);
+	}
+
+	@Override
+	public List<Estado> listar() {
+		return manager.createQuery("from Estado", Estado.class).getResultList();
+	}
+
+}
