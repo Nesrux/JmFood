e17c7045935618e419323f58f6d906d8daa29d67
Nesrux <joaomarcosdevs@gmail.com>
2023-03-16 18:27:32 -0300

    Adicionando um mapeamento por ID

--- src/main/java/com/nesrux/jmfood/api/controller/CozinhaController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/CozinhaController.java
@@ -3,21 +3,29 @@ package com.nesrux.jmfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nesrux.jmfood.domain.model.Cozinha;
 import com.nesrux.jmfood.domain.repository.CozinhaRepository;
+
 @RestController
 @RequestMapping("/cozinhas")
 public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
+	}
+
+	@GetMapping("/{cozinhaId}")
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
+		return cozinhaRepository.buscar(cozinhaId);
 	}
 
 }
