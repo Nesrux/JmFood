da491b0c27f539947954c3808e819f2afcc64f7c
Nesrux <joaomarcosdevs@gmail.com>
2023-03-16 18:32:08 -0300

    buscar estado por ID

--- src/main/java/com/nesrux/jmfood/api/controller/EstadoController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/EstadoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,9 @@ public class EstadoController {
 	@GetMapping
 	public List<Estado> listar(){
 		return estadoRepository.listar();
+	}
+	@GetMapping("/{estadoId}")
+	public Estado buscar(@PathVariable Long estadoId) {
+		return estadoRepository.buscar(estadoId);
 	}
 }
