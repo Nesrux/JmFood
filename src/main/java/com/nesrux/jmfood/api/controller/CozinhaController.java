3bab13d73434446b6bb091e51f3d564ea66e7194
Nesrux <joaomarcosdevs@gmail.com>
2023-03-27 17:06:43 -0300

    Adicionando endpoint para adicionar cozinhas

--- src/main/java/com/nesrux/jmfood/api/controller/CozinhaController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/CozinhaController.java
@@ -3,11 +3,15 @@ package com.nesrux.jmfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nesrux.jmfood.api.model.CozinhasXmlWrapper;
@@ -38,6 +42,11 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinha);
 		}
 		return ResponseEntity.notFound().build();
+	}
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void adicionar (@RequestBody Cozinha cozinha) {
+		cozinhaRepository.salvar(cozinha);
 	}
 
 }
