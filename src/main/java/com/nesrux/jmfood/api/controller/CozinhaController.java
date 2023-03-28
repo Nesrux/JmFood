09fe259ba3052cf03f3b77d47c972c01b9b957d9
Nesrux <joaomarcosdevs@gmail.com>
2023-03-28 17:11:18 -0300

    adicionando endpoint para deletar cozinha

--- src/main/java/com/nesrux/jmfood/api/controller/CozinhaController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/CozinhaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,6 +65,12 @@ public class CozinhaController {
 		}
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+
+	@DeleteMapping
+	public void deletar(Long id) {
+		Cozinha cozihaAtual = cozinhaRepository.buscar(id);
+		cozinhaRepository.remover(cozihaAtual);
 	}
 
 }
