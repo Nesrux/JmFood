190264349bfd21e275f65eba647451b06cf2dacb
Nesrux <joaomarcosdevs@gmail.com>
2023-03-29 19:30:57 -0300

    Adicionando o endpoint de atualizar um restaurante

--- src/main/java/com/nesrux/jmfood/api/controller/RestauranteController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/RestauranteController.java
@@ -2,12 +2,14 @@ package com.nesrux.jmfood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,8 +48,24 @@ public class RestauranteController {
 	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
 		try {
 			restaurante = restauranteService.savar(restaurante);
-			
+
 			return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+		try {
+			Restaurante restauranteAtual = repository.buscar(restauranteId);
+			if (restauranteAtual != null) {
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+				restauranteAtual = restauranteService.savar(restauranteAtual);
+
+				return ResponseEntity.ok().body(restauranteAtual);
+			}
+			return ResponseEntity.notFound().build();
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
