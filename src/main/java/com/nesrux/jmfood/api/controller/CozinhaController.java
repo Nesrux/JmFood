18f28f8b45ab30c4b0171cbd64822a72d5e8c342
Nesrux <joaomarcosdevs@gmail.com>
2023-03-29 16:42:55 -0300

    Removendo o suporte oara XML do projeto

--- src/main/java/com/nesrux/jmfood/api/controller/CozinhaController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/CozinhaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nesrux.jmfood.api.model.CozinhasXmlWrapper;
 import com.nesrux.jmfood.domain.exception.EntidadeEmUsoException;
 import com.nesrux.jmfood.domain.exception.EntidadeNaoEncontradaException;
 import com.nesrux.jmfood.domain.model.Cozinha;
@@ -36,11 +34,6 @@ public class CozinhaController {
 	@GetMapping()
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
-	}
-
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml() {
-		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 
 	@GetMapping("/{cozinhaId}")
@@ -67,7 +60,7 @@ public class CozinhaController {
 			// Importante
 			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
 
-			cozinhaRepository.salvar(cozinhaAtual);
+			cozinhaService.salvar(cozinhaAtual);
 			return ResponseEntity.ok(cozinhaAtual);
 		}
 
