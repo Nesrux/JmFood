d75ca25fbacde6f91da783dfdfbf1cb4e3ce9947
Nesrux <joaomarcosdevs@gmail.com>
2023-03-30 16:56:03 -0300

    Criando a classe service de estado

--- src/main/java/com/nesrux/jmfood/domain/service/CadastroEstadoService.java
+++ src/main/java/com/nesrux/jmfood/domain/service/CadastroEstadoService.java
@@ -1,5 +1,36 @@
 package com.nesrux.jmfood.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.nesrux.jmfood.domain.exception.EntidadeEmUsoException;
+import com.nesrux.jmfood.domain.exception.EntidadeNaoEncontradaException;
+import com.nesrux.jmfood.domain.model.Estado;
+import com.nesrux.jmfood.domain.repository.EstadoRepository;
+
+@Service
 public class CadastroEstadoService {
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	public Estado Salvar(Estado estado) {
+		return estadoRepository.salvar(estado);
+	}
+
+	public void excluir(Long estadoId) {
+		try {
+			Estado estado = estadoRepository.buscar(estadoId);
+			estadoRepository.remover(estado);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe um cadastro de cozinha com o código %d", estadoId));
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+					String.format("Cozinha de código %d não pode ser removida, pois está em uso", estadoId));
+		}
+	}
 
 }
