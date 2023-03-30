093c7215402358a286532a1f5327b7da53146399
Nesrux <joaomarcosdevs@gmail.com>
2023-03-30 17:11:23 -0300

    Adicionando endpoit para atualizar os estados

--- src/main/java/com/nesrux/jmfood/api/controller/EstadoController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/EstadoController.java
@@ -7,17 +7,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nesrux.jmfood.domain.model.Estado;
 import com.nesrux.jmfood.domain.repository.EstadoRepository;
+import com.nesrux.jmfood.domain.service.CadastroEstadoService;
 
 @RestController
 @RequestMapping("/estados")
 public class EstadoController {
 	@Autowired
 	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CadastroEstadoService estadoService;
 
 	@GetMapping
 	public List<Estado> listar() {
@@ -33,5 +39,11 @@ public class EstadoController {
 		}
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+
+	@PostMapping
+	public ResponseEntity<Estado> adicionar(@RequestBody Estado estado) {
+		estado = estadoService.Salvar(estado);
+		return ResponseEntity.status(HttpStatus.CREATED).body(estado);
 	}
 }
