ba1cf1b7d983a4080baa73e0bdea6e29a140e96e
Nesrux <joaomarcosdevs@gmail.com>
2023-03-30 17:20:53 -0300

    EndPoint para atualizar um estado

--- src/main/java/com/nesrux/jmfood/api/controller/EstadoController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/EstadoController.java
@@ -2,12 +2,14 @@ package com.nesrux.jmfood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,4 +48,19 @@ public class EstadoController {
 		estado = estadoService.Salvar(estado);
 		return ResponseEntity.status(HttpStatus.CREATED).body(estado);
 	}
+
+	@PutMapping("{estadoId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long estadoId, @RequestBody Estado estado) {
+		Estado estadoAtual = estadoRepository.buscar(estadoId);
+
+		if (estadoAtual != null) {
+
+			BeanUtils.copyProperties(estado, estadoAtual, "id");
+			estadoAtual = estadoService.Salvar(estadoAtual);
+
+			return ResponseEntity.ok(estadoAtual);
+		}
+		return ResponseEntity.notFound().build();
+	}
+
 }
