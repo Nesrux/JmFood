e969bb261dae3569d98469bf8a679d1868ce81a7
Nesrux <joaomarcosdevs@gmail.com>
2023-03-30 16:58:56 -0300

    refatorando busca de um unico estado

--- src/main/java/com/nesrux/jmfood/api/controller/EstadoController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/EstadoController.java
@@ -3,6 +3,8 @@ package com.nesrux.jmfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +18,20 @@ import com.nesrux.jmfood.domain.repository.EstadoRepository;
 public class EstadoController {
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@GetMapping
-	public List<Estado> listar(){
+	public List<Estado> listar() {
 		return estadoRepository.listar();
 	}
+
 	@GetMapping("/{estadoId}")
-	public Estado buscar(@PathVariable Long estadoId) {
-		return estadoRepository.buscar(estadoId);
+	public ResponseEntity<Estado> buscar(@PathVariable Long estadoId) {
+		Estado estado = estadoRepository.buscar(estadoId);
+
+		if (estado != null) {
+			return ResponseEntity.ok(estado);
+		}
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 }
