0ce977639dfd7ab5d5b2577852f7d1e8d2b29208
Nesrux <joaomarcosdevs@gmail.com>
2023-04-02 17:51:28 -0300

    aprendendo mais sobre JPA

--- src/main/java/com/nesrux/jmfood/api/controller/TesteController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/TesteController.java
@@ -1,16 +1,24 @@
 package com.nesrux.jmfood.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nesrux.jmfood.domain.model.Cozinha;
+import com.nesrux.jmfood.domain.repository.CozinhaRepository;
 
 @RestController
 @RequestMapping("/teste")
 public class TesteController {
-	//@Autowired
-	//private CozinhaRepository repository;
+	@Autowired
+	private CozinhaRepository repository;
 
-//	@GetMapping("/cozinhas/por-nome")
-//	public List<Cozinha> cozinhaPorNome(@RequestParam String nome) {
-//		return repository.buscarNome(nome);
-//	}
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhaPorNome(@RequestParam String nome) {
+		return repository.findLikeByNome(nome);
+	}
 }
--- src/main/java/com/nesrux/jmfood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/nesrux/jmfood/domain/repository/CozinhaRepository.java
@@ -1,10 +1,13 @@
 package com.nesrux.jmfood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nesrux.jmfood.domain.model.Cozinha;
 
-
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
+
+	List<Cozinha> findLikeByNome(String nome);
 
 }
