ba8f9220b5532d10aabfac11a2db1aec930be02f
Nesrux <joaomarcosdevs@gmail.com>
2023-04-04 16:31:47 -0300

    usando queries em jpql personalizado

--- src/main/java/com/nesrux/jmfood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/nesrux/jmfood/domain/repository/RestauranteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.nesrux.jmfood.domain.model.Restaurante;
 
@@ -12,8 +13,11 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 	// alem do prefixo find, existe também o query, stream, get que funcionam
 	// exatamente igual
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
-
-	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+	
+	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	List <Restaurante> acharPorNomeEId(String nome, Long id);
+	
+//	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 
 	Optional<Restaurante> findFristRestauranteByNomeContaining(String nome);
 
