919324af53558dade5ace145609720149386b630
Nesrux <joaomarcosdevs@gmail.com>
2023-04-10 18:41:56 -0300

    implementando a Criteria API no peojeto

--- src/main/java/com/nesrux/jmfood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/nesrux/jmfood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,15 +1,15 @@
 package com.nesrux.jmfood.infrastructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import com.nesrux.jmfood.domain.model.Restaurante;
 import com.nesrux.jmfood.domain.repository.CustomizedRestauranteRepository;
@@ -23,31 +23,14 @@ public class RestauranteRepositoryImpl implements CustomizedRestauranteRepositor
 
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-		var jpql = new StringBuilder();
-		jpql.append("from Restaurante where 0 = 0 ");
-		
-		var parametros = new HashMap<String, Object>();
-		
-		if (StringUtils.hasLength(nome)) {
-			jpql.append("and nome like :nome ");
-			parametros.put("nome", "%" + nome + "%");
-		}
-		
-		if (taxaFreteInicial != null) {
-			jpql.append("and taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaFreteInicial);
-		}
-		
-		if (taxaFreteFinal != null) {
-			jpql.append("and taxaFrete <= :taxaFinal ");
-			parametros.put("taxaFinal", taxaFreteFinal);
-		}
-		
-		TypedQuery<Restaurante> query = manager
-				.createQuery(jpql.toString(), Restaurante.class);
-		
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
 
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		criteria.from(Restaurante.class);//FROM Restaurante
+
+		TypedQuery<Restaurante> query =  manager.createQuery(criteria);
+		
 		return query.getResultList();
+		
 	}
 }
