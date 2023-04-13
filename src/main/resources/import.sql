600c72071d38b2d67061bba997062d99da411a19
Nesrux <joaomarcosdevs@gmail.com>
2023-04-13 13:51:38 -0300

    Adicionando dados ao arquivo Import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -29,3 +29,5 @@ insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
