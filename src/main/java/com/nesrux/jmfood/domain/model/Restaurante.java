5d926e9b0b9631c5e898634fb2afc6a8ea661fb6
Nesrux <joaomarcosdevs@gmail.com>
2023-04-13 13:47:22 -0300

    Mapeando o relacionamento de FormaPagamento para restaurante

--- src/main/java/com/nesrux/jmfood/domain/model/Restaurante.java
+++ src/main/java/com/nesrux/jmfood/domain/model/Restaurante.java
@@ -1,6 +1,8 @@
 package com.nesrux.jmfood.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -42,5 +46,11 @@ public class Restaurante {
 	// Com a opção de renomear a coluna
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+	
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento", 
+	joinColumns = @JoinColumn(name = "restaurante_id"),
+	inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamentos = new ArrayList<>();
 
 }
