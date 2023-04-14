5ad11bfcbd5dc7d0caa5fe4453f025331c008b7d
Nesrux <joaomarcosdevs@gmail.com>
2023-04-14 17:41:57 -0300

    mapeando as entidades com DATETIME

--- src/main/java/com/nesrux/jmfood/domain/model/Restaurante.java
+++ src/main/java/com/nesrux/jmfood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.nesrux.jmfood.domain.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -40,13 +44,23 @@ public class Restaurante {
 	private Cozinha cozinha;
 	
 	@JsonIgnore
+	@Embedded
+	private Endereco endereco;
+	
+	@CreationTimestamp //Sempre que a entidade for criada, ele vaiatualizar essa coluna
+	@Column(nullable = false , columnDefinition = "datetime")
+	private LocalDateTime dataCadastro;
+	
+	@UpdateTimestamp//sempre que a entidade for atualizada, ele vai criar essa coluna!
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataAtualizacao;
+	
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento", 
 	joinColumns = @JoinColumn(name = "restaurante_id"),
 	inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamentos = new ArrayList<>();
 	
-	@JsonIgnore
-	@Embedded
-	private Endereco endereco;
+
 }
