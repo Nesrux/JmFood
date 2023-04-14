e6c6d5e0f68c04cae9f52844297f855729458253
Nesrux <joaomarcosdevs@gmail.com>
2023-04-14 19:15:46 -0300

    desafio feito

--- src/main/java/com/nesrux/jmfood/domain/model/Produto.java
+++ src/main/java/com/nesrux/jmfood/domain/model/Produto.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -35,6 +37,7 @@ public class Produto {
 	@Column(nullable = false)
 	private boolean ativo;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(nullable = false)
 	private Restaurante restaurante;
