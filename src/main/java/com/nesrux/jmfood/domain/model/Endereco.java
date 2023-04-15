53038acf61e714d5b71871bef50ddc64b8caaecf
Nesrux <joaomarcosdevs@gmail.com>
2023-04-15 19:23:05 -0300

    alterando o EAGER para Lazy dentro da classe Restaurantes

--- src/main/java/com/nesrux/jmfood/domain/model/Endereco.java
+++ src/main/java/com/nesrux/jmfood/domain/model/Endereco.java
@@ -2,6 +2,7 @@ package com.nesrux.jmfood.domain.model;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
@@ -12,22 +13,21 @@ import lombok.Data;
 public class Endereco {
 	@Column(name = "endereco_cep")
 	private String cep;
-	
+
 	@Column(name = "endereco_logradouro")
 	private String logradouro;
-	
+
 	@Column(name = "endereco_numero")
 	private String numero;
-	
+
 	@Column(name = "endereco_complemento")
 	private String complemento;
-	
+
 	@Column(name = "endereco_bairro")
 	private String bairro;
-	
-	@ManyToOne
+
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "endereco_cidade_id")
 	private Cidade cidade;
-	
-	
+
 }
