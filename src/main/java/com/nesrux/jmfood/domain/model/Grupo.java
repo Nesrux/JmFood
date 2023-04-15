012b5a9b46cfede998a4459870f601444ee749b0
Nesrux <joaomarcosdevs@gmail.com>
2023-04-15 17:33:10 -0300

    Mapeando a classe Grupo

---
+++ src/main/java/com/nesrux/jmfood/domain/model/Grupo.java
@@ -0,0 +1,24 @@
+package com.nesrux.jmfood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+public class Grupo {
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+}
