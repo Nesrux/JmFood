3dc0fb266be28fd329f3f1fdb7a9a91e2de6efbb
Nesrux <joaomarcosdevs@gmail.com>
2023-04-17 18:12:50 -0300

    arrumando erro na migration V012

--- src/main/resources/db/migration/V012__Cria-tabela-usuario-grupo.sql
+++ src/main/resources/db/migration/V012__Cria-tabela-usuario-grupo.sql
@@ -1,9 +1,6 @@
-create table usuario (
-	id bigint not null auto_increment,
-	nome varchar(80) not null,
-	email varchar(255) not null,
-	senha varchar(255) not null,
-	data_cadastro datetime not null,
+create table usuario_grupo (
+	usuario_id bigint not null,
+	grupo_id bigint not null,
 	
-	primary key (id)
+	primary key (usuario_id, grupo_id)
 ) engine=InnoDB default charset=utf8;
