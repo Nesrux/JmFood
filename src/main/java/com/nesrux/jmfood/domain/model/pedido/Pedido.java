25c5807a7d002b1dcd5d984f1ffcddf9f01ea4eb
Nesrux <joaomarcosdevs@gmail.com>
2023-04-18 15:52:33 -0300

    criação da entidade Pedido completo

--- src/main/java/com/nesrux/jmfood/domain/model/pedido/Pedido.java
+++ src/main/java/com/nesrux/jmfood/domain/model/pedido/Pedido.java
@@ -1,12 +1,24 @@
 package com.nesrux.jmfood.domain.model.pedido;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.nesrux.jmfood.domain.model.endereco.Endereco;
+import com.nesrux.jmfood.domain.model.user.Usuario;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,18 +33,29 @@ public class Pedido {
 	private Long id;
 
 	private BigDecimal subtotal;
-
 	private BigDecimal taxaFrete;
-
 	private BigDecimal valorTotal;
 
+	@CreationTimestamp
+	private LocalDate dataCriacao;
+	
 	private LocalDateTime dataConfirmacao;
-
 	private LocalDateTime dataCancelamento;
-
 	private LocalDateTime dataEntrega;
 
 	private StatusPedido statusPedido;
 
-	private ItemPedido itemPedido;
+	@Embedded
+	private Endereco endereco;
+
+	@ManyToOne
+	@JoinColumn(name = "usuario_cliente_id", nullable = false)
+	private Usuario cliente;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private FormaPagamento formaPagamento;
+
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itemPedidos = new ArrayList<>();
 }
