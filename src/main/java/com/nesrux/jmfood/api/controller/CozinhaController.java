95f186de153c5227495854a8ce5334d7ffab7e28
Nesrux <joaomarcosdevs@gmail.com>
2023-04-21 18:37:07 -0300

    refatorando os códigos do service e do controller de cozinha

--- src/main/java/com/nesrux/jmfood/api/controller/CozinhaController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/CozinhaController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,17 +46,14 @@ public class CozinhaController {
 	}
 
 	@PutMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+	public Cozinha atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
 		Cozinha cozinhaAtual = cozinhaService.buscaOuFalha(cozinhaId);
-		if (cozinhaAtual != null) {
-			// Importante
-			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
-
-			Cozinha cozinhaSalva = cozinhaService.salvar(cozinhaAtual);
-			return ResponseEntity.ok(cozinhaSalva);
-		}
-
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		
+		// Importante
+		BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+		Cozinha cozinhaSalva = cozinhaService.salvar(cozinhaAtual);
+	
+		return cozinhaSalva;
 	}
 
 	@DeleteMapping("/{cozinhaId}")
--- src/main/java/com/nesrux/jmfood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/nesrux/jmfood/domain/service/CadastroCozinhaService.java
@@ -12,6 +12,10 @@ import com.nesrux.jmfood.domain.repository.CozinhaRepository;
 
 @Service
 public class CadastroCozinhaService {
+	private static final String MSG_COZINHA_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
+
+	private static final String MSG_COZINHA_NAO_ENCONTRADA = "Não existe um cadastro de cozinha com o código %d";
+
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 
@@ -24,19 +28,19 @@ public class CadastroCozinhaService {
 			cozinhaRepository.deleteById(cozinhaId);
 		} catch (EmptyResultDataAccessException e) {
 			throw new EntidadeNaoEncontradaException(
-					String.format("Não existe um cadastro de cozinha com o código %d", cozinhaId));
+					String.format(MSG_COZINHA_NAO_ENCONTRADA, cozinhaId));
 		}
 
 		catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-					String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
+					String.format(MSG_COZINHA_EM_USO, cozinhaId));
 		}
 	}
 
 	public Cozinha buscaOuFalha(Long cozinhaId) {
 		return cozinhaRepository.findById(cozinhaId).orElseThrow(() -> 
 			new EntidadeNaoEncontradaException(
-				String.format("Não existe um cadastro de cozinha com o código %d", cozinhaId)));
+				String.format(MSG_COZINHA_NAO_ENCONTRADA, cozinhaId)));
 	}
 
 }
