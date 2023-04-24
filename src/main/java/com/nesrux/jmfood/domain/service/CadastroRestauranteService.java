16a1948ae331d32da0e3d392f9ccb1edd1c68293
Nesrux <joaomarcosdevs@gmail.com>
2023-04-24 14:39:05 -0300

    refatorando o código do service de restaurante

--- src/main/java/com/nesrux/jmfood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/nesrux/jmfood/domain/service/CadastroRestauranteService.java
@@ -1,32 +1,45 @@
 package com.nesrux.jmfood.domain.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nesrux.jmfood.domain.exception.EntidadeNaoEncontradaException;
 import com.nesrux.jmfood.domain.model.restaurante.Cozinha;
 import com.nesrux.jmfood.domain.model.restaurante.Restaurante;
-import com.nesrux.jmfood.domain.repository.CozinhaRepository;
 import com.nesrux.jmfood.domain.repository.RestauranteRepository;
 
 @Service
 public class CadastroRestauranteService {
 
 	@Autowired
-	private RestauranteRepository repository;
+	private RestauranteRepository restauranteRepository;
 
 	@Autowired
-	private CozinhaRepository cozinhaRepository;
+	private CadastroCozinhaService cozinhaService;
 
 	public Restaurante salvar(Restaurante restaurante) {
 		Long cozinhaId = restaurante.getCozinha().getId();
-		Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
-				.orElseThrow(() -> 	new EntidadeNaoEncontradaException(String
-						.format("Não existe cadastro com o código %d", cozinhaId ))
-						);
+
+		Cozinha cozinha = cozinhaService.buscaOuFalha(cozinhaId);
+
 		restaurante.setCozinha(cozinha);
-		return repository.save(restaurante);
+
+		return restauranteRepository.save(restaurante);
+	}
+
+	@SuppressWarnings("unused")
+	private List<Restaurante> acharTodos() {
+		return restauranteRepository.findAll();
+	}
+
+	public Restaurante acharOuFalhar(Long restauranteId) {
+		Restaurante restaurante = restauranteRepository.findById(restauranteId)
+				.orElseThrow(() -> new EntidadeNaoEncontradaException(
+						String.format("Não existe restaurante com o id: %d", restauranteId)));
+
+		return restaurante;
 	}
 
 }
- 
