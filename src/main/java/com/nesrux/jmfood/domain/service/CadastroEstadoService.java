72cf5889b894030ebd55e15d77ff60f2aad8b439
Nesrux <joaomarcosdevs@gmail.com>
2023-04-24 13:48:27 -0300

    refatoração do código dos endpoits ESTADo e service de ESTADO completos

--- src/main/java/com/nesrux/jmfood/domain/service/CadastroEstadoService.java
+++ src/main/java/com/nesrux/jmfood/domain/service/CadastroEstadoService.java
@@ -1,7 +1,5 @@
 package com.nesrux.jmfood.domain.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -14,10 +12,10 @@ import com.nesrux.jmfood.domain.repository.EstadoRepository;
 
 @Service
 public class CadastroEstadoService {
-	private static final String MSG_COZINHA_NAO_ENCONTRADA = "Não existe um cadastro de cozinha com o código %d";
+	private static final String MSG_ESTADO_NAO_ENCONTRADA = "Não existe um cadastro de estado com o código %d";
 
-	private static final String MSG_COZINHA_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
-	
+	private static final String MSG_ESTADO_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
+
 	@Autowired
 	private EstadoRepository estadoRepository;
 
@@ -27,21 +25,17 @@ public class CadastroEstadoService {
 
 	public void excluir(Long estadoId) {
 		try {
-			Optional<Estado> estado = estadoRepository.findById(estadoId);
-			estadoRepository.delete(estado.get());
+			estadoRepository.deleteById(estadoId);
 
 		} catch (EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(
-					String.format(MSG_COZINHA_NAO_ENCONTRADA, estadoId));
+			throw new EntidadeNaoEncontradaException(String.format(MSG_ESTADO_NAO_ENCONTRADA, estadoId));
 		} catch (DataIntegrityViolationException e) {
-			throw new EntidadeEmUsoException(
-					String.format(MSG_COZINHA_EM_USO, estadoId));
+			throw new EntidadeEmUsoException(String.format(MSG_ESTADO_EM_USO, estadoId));
 		}
 	}
-	
+
 	public Estado achaOuFaha(Long estadoId) {
 		return estadoRepository.findById(estadoId)
-				.orElseThrow(() -> 
-				new EntidadeNaoEncontradaException(String.format(MSG_COZINHA_EM_USO, estadoId)));
+				.orElseThrow(() -> new EntidadeNaoEncontradaException(String.format(MSG_ESTADO_EM_USO, estadoId)));
 	}
 }
