0dd10ef0bf68dea6945416f5f59bceeab84d2531
Nesrux <joaomarcosdevs@gmail.com>
2023-04-26 17:03:13 -0300

    implementando o padrao de resposta RFC 7807

--- src/main/java/com/nesrux/jmfood/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/nesrux/jmfood/api/exceptionHandler/ApiExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.nesrux.jmfood.api.exceptionHandler;
 
-import java.time.LocalDateTime;
-
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.nesrux.jmfood.domain.exception.NegocioException;
@@ -13,52 +13,62 @@ import com.nesrux.jmfood.domain.exception.negocioException.EntidadeEmUsoExceptio
 import com.nesrux.jmfood.domain.exception.negocioException.EntidadeNaoEncontradaException;
 
 @ControllerAdvice
-public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	/*
-	 * Essa classe ResponseEntityExceptionHandler é uma classe utilitaria que trata todas as possiveis exceptions do spring MVC
-	 * como a classe httpMediaTypeNotaceppt
-	 * */
-	
-	
+	 * Essa classe ResponseEntityExceptionHandler é uma classe utilitaria que
+	 * trata todas as possiveis exceptions do spring MVC como a classe
+	 * httpMediaTypeNotaceppt
+	 */
+
 	/*
 	 * Essa anotação controllerAdvice "fala para o spring" que essa classe vai
 	 * capturat todas as execoes
 	 */
 
 	/*
-	 * a anotação @ExceptionHandler captura as execoes mandaddas, ela é anotada em
-	 * cima dos métodos e pode ser utilizado para criar respostas de erros dinamicas
-	 * Ele só captura as exceptions geradas nesse controlador
+	 * a anotação @ExceptionHandler captura as execoes mandaddas, ela é anotada
+	 * em cima dos métodos e pode ser utilizado para criar respostas de erros
+	 * dinamicas Ele só captura as exceptions geradas nesse controlador
 	 */
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(EntidadeNaoEncontradaException e) {
-		ErroApi erro = ErroApi.builder()
-				.datahora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	public ResponseEntity<?> tratarNegocioException(
+			EntidadeNaoEncontradaException e, WebRequest request) {
+
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(),
+				HttpStatus.BAD_REQUEST, request);
 	}
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontrada(EntidadeNaoEncontradaException e) {
-		ErroApi erro = ErroApi.builder()
-				.datahora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
+	public ResponseEntity<?> tratarEntidadeNaoEncontrada(
+			EntidadeNaoEncontradaException e, WebRequest request) {
 
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(),
+				HttpStatus.NOT_FOUND, request);
 	}
-	
+
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeNaoEncontradaException e) {
-		ErroApi erro = ErroApi.builder()
-				.datahora(LocalDateTime.now())
-				.mensagem(e.getMessage())
-				.build();
+	public ResponseEntity<?> tratarEntidadeEmUsoException(
+			EntidadeNaoEncontradaException e, WebRequest request) {
 
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(),
+				HttpStatus.NOT_FOUND, request);
 	}
-	
-		
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex,
+			Object body, HttpHeaders headers, HttpStatus status,
+			WebRequest request) {
+
+		if (body == null) {
+			body = ErroApi.builder().title(status.getReasonPhrase())
+					.status(status.value()).build();
+		} else if (body instanceof String) {
+			body = ErroApi.builder().title((String) body).status(status.value())
+					.build();
+
+		}
+		return super.handleExceptionInternal(ex, body, headers, status,
+				request);
+	}
+
 }
--- src/main/java/com/nesrux/jmfood/api/exceptionHandler/ErroApi.java
+++ src/main/java/com/nesrux/jmfood/api/exceptionHandler/ErroApi.java
@@ -1,14 +1,13 @@
 package com.nesrux.jmfood.api.exceptionHandler;
 
-import java.time.LocalDateTime;
-
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ErroApi {
-
-	private LocalDateTime datahora;
-	private String mensagem;
+	private Integer status;
+	private String type;
+	private String title;
+	private String detail;
 }
