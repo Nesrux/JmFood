18afd4c9cfe57cbf9446d247b134a03e4d34776f
Nesrux <joaomarcosdevs@gmail.com>
2023-04-26 13:59:32 -0300

    removendo as anotações de responseStatus das classes de exceptions

--- src/main/java/com/nesrux/jmfood/domain/exception/negocioException/EntidadeEmUsoException.java
+++ src/main/java/com/nesrux/jmfood/domain/exception/negocioException/EntidadeEmUsoException.java
@@ -1,11 +1,7 @@
 package com.nesrux.jmfood.domain.exception.negocioException;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import com.nesrux.jmfood.domain.exception.NegocioException;
 
-@ResponseStatus(value = HttpStatus.CONFLICT)
 public class EntidadeEmUsoException extends NegocioException {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/nesrux/jmfood/domain/exception/negocioException/EntidadeNaoEncontradaException.java
+++ src/main/java/com/nesrux/jmfood/domain/exception/negocioException/EntidadeNaoEncontradaException.java
@@ -1,11 +1,7 @@
 package com.nesrux.jmfood.domain.exception.negocioException;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import com.nesrux.jmfood.domain.exception.NegocioException;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public abstract class EntidadeNaoEncontradaException extends NegocioException {
 	private static final long serialVersionUID = 1L;
 
