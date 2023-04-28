9eb8d1f01fc65583b99782648d82cc3781ecab84
Nesrux <joaomarcosdevs@gmail.com>
2023-04-28 18:01:42 -0300

    Sobrescrevendo o método handleHttpMessageNotReadable da classe pai

--- src/main/java/com/nesrux/jmfood/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/nesrux/jmfood/api/exceptionHandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.nesrux.jmfood.api.exceptionHandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,106 +15,111 @@ import com.nesrux.jmfood.domain.exception.negocioException.EntidadeNaoEncontrada
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
-	/*
-	 * Essa classe ResponseEntityExceptionHandler é uma classe utilitaria que
-	 * trata todas as possiveis exceptions do spring MVC como a classe
-	 * httpMediaTypeNotaceppt
+    /*
+     * Essa classe ResponseEntityExceptionHandler é uma classe utilitaria que trata
+     * todas as possiveis exceptions do spring MVC como a classe
+     * httpMediaTypeNotaceppt
+     */
+
+    /*
+     * Essa anotação controllerAdvice "fala para o spring" que essa classe vai
+     * capturat todas as execoes
+     */
+
+    /*
+     * a anotação @ExceptionHandler captura as execoes mandaddas, ela é anotada em
+     * cima dos métodos e pode ser utilizado para criar respostas de erros dinamicas
+     * Ele só captura as exceptions geradas nesse controlador
+     */
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<?> handleNegocioException(EntidadeNaoEncontradaException e, WebRequest request) {
+
+	// return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(),
+	// HttpStatus.BAD_REQUEST, request);
+
+	HttpStatus status = HttpStatus.NOT_FOUND;
+	TipoProblema tipoProblema = TipoProblema.ENTIDADE_EM_USO;
+	String detail = e.getMessage();
+	ErroApi erro = criacaoDeBilderProblema(status, tipoProblema, detail).build();
+
+	return handleExceptionInternal(e, erro, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<?> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException e, WebRequest request) {
+
+	HttpStatus status = HttpStatus.NOT_FOUND;
+	TipoProblema tipoProblema = TipoProblema.ENTIDADE_NAO_ENCONTRADA;
+	String detail = e.getMessage();
+
+	ErroApi erro = criacaoDeBilderProblema(status, tipoProblema, detail).build();
+	/**
+	 * Ambos códigos são o mesmo e funcionam da mesma forma, a diferença é que fica
+	 * mais eleante e mais pratico para manutenção!
 	 */
 
-	/*
-	 * Essa anotação controllerAdvice "fala para o spring" que essa classe vai
-	 * capturat todas as execoes
-	 */
+	// ErroApi erro = ErroApi.builder().status(status.value())
+	// .type("https://jmfood.com.br/entidade-nao-encontrada")
+	// .title("Entidade Não Encontrada").detail(detail).build();
 
-	/*
-	 * a anotação @ExceptionHandler captura as execoes mandaddas, ela é anotada
-	 * em cima dos métodos e pode ser utilizado para criar respostas de erros
-	 * dinamicas Ele só captura as exceptions geradas nesse controlador
-	 */
-	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> handleNegocioException(
-			EntidadeNaoEncontradaException e, WebRequest request) {
+	return handleExceptionInternal(e, erro, new HttpHeaders(), status, request);
+    }
 
-		// return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(),
-		// HttpStatus.BAD_REQUEST, request);
+    @ExceptionHandler(EntidadeEmUsoException.class)
+    public ResponseEntity<?> hendleEntidadeEmUsoException(EntidadeNaoEncontradaException e, WebRequest request) {
+	HttpStatus status = HttpStatus.CONFLICT;
+	TipoProblema tipoProblema = TipoProblema.ENTIDADE_EM_USO;
+	String detail = e.getMessage();
 
-		HttpStatus status = HttpStatus.NOT_FOUND;
-		TipoProblema tipoProblema = TipoProblema.ENTIDADE_EM_USO;
-		String detail = e.getMessage();
-		ErroApi erro = criacaoDeBilderProblema(status, tipoProblema, detail)
-				.build();
+	ErroApi erro = criacaoDeBilderProblema(status, tipoProblema, detail).build();
 
-		return handleExceptionInternal(e, erro, new HttpHeaders(), status,
-				request);
+	return handleExceptionInternal(e, erro, null, status, request);
+    }
+    // ======================= MÉTODOS PRIVADOS ========================== \\
+
+    /*
+     * Ele retorna um erroAPIBuider uma classe criada pelo lombok que usa o padrão
+     * Builder de projeto. como esse método não possui um .build no final do método,
+     * ele vai retornar uma pseudo intancia de ErroAPI, que vai poder ser editado em
+     * qualquer método que chame ele
+     */
+
+    private ErroApi.ErroApiBuilder criacaoDeBilderProblema(HttpStatus status, TipoProblema tipoProblema,
+	    String detail) {
+	return ErroApi.builder().status(status.value()).type(tipoProblema.getUri()).title(tipoProblema.getTitulo())
+		.detail(detail);
+    }
+
+    // ============== MÉTODOS SOBRESCRITOS DA CLASSE PAI =======================
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+	    HttpHeaders headers, HttpStatus status, WebRequest request) {
+	TipoProblema tipoProblema = TipoProblema.MENSAGEM_INCOMPREENSIVEL;
+
+	String detail = "o corpo da requisição esta invalido. Verifique o erro de sintaxe.";
+
+	ErroApi erro = criacaoDeBilderProblema(status, tipoProblema, detail).build();
+
+	return handleExceptionInternal(ex, erro, new HttpHeaders(), status, request);
+
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+	    HttpStatus status, WebRequest request) {
+
+	if (body == null) {
+	    body = ErroApi.builder().title(status.getReasonPhrase()).status(status.value()).build();
+	} else if (body instanceof String) {
+	    body = ErroApi.builder().title((String) body).status(status.value()).build();
 	}
-
-	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> handleEntidadeNaoEncontrada(
-			EntidadeNaoEncontradaException e, WebRequest request) {
-
-		HttpStatus status = HttpStatus.NOT_FOUND;
-		TipoProblema tipoProblema = TipoProblema.ENTIDADE_NAO_ENCONTRADA;
-		String detail = e.getMessage();
-
-		ErroApi erro = criacaoDeBilderProblema(status, tipoProblema, detail)
-				.build();
-		/**
-		 * Ambos códigos são o mesmo e funcionam da mesma forma, a diferença é
-		 * que fica mais eleante e mais pratico para manutenção!
-		 */
-
-		// ErroApi erro = ErroApi.builder().status(status.value())
-		// .type("https://jmfood.com.br/entidade-nao-encontrada")
-		// .title("Entidade Não Encontrada").detail(detail).build();
-
-		return handleExceptionInternal(e, erro, new HttpHeaders(), status,
-				request);
+	// Verifica se o cabeçalho esta nulo, se estiver ele atribui um
+	// cabeçaçho padrao
+	if (headers == null) {
+	    headers = new HttpHeaders();
 	}
+	return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
 
-	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> hendleEntidadeEmUsoException(
-			EntidadeNaoEncontradaException e, WebRequest request) {
-		HttpStatus status = HttpStatus.CONFLICT;
-		TipoProblema tipoProblema = TipoProblema.ENTIDADE_EM_USO;
-		String detail = e.getMessage();
-
-		ErroApi erro = criacaoDeBilderProblema(status, tipoProblema, detail)
-				.build();
-
-		return handleExceptionInternal(e, erro, null, status, request);
-	}
-
-	@Override
-	protected ResponseEntity<Object> handleExceptionInternal(Exception ex,
-			Object body, HttpHeaders headers, HttpStatus status,
-			WebRequest request) {
-
-		if (body == null) {
-			body = ErroApi.builder().title(status.getReasonPhrase())
-					.status(status.value()).build();
-		} else if (body instanceof String) {
-			body = ErroApi.builder().title((String) body).status(status.value())
-					.build();
-		}
-		// Verifica se o cabeçalho esta nulo, se estiver ele atribui um
-		// cabeçaçho padrao
-		if (headers == null) {
-			headers = new HttpHeaders();
-		}
-		return super.handleExceptionInternal(ex, body, headers, status,
-				request);
-	}
-
-	/*
-	 * Ele retorna um erroAPIBuider uma classe criada pelo lombok que usa o
-	 * padrão Builder de projeto. como esse método não possui um .build no final
-	 * do método, ele vai retornar uma pseudo intancia de ErroAPI, que vai poder
-	 * ser editado em qualquer método que chame ele
-	 */
-	private ErroApi.ErroApiBuilder criacaoDeBilderProblema(HttpStatus status,
-			TipoProblema tipoProblema, String detail) {
-		return ErroApi.builder().status(status.value())
-				.type(tipoProblema.getUri()).title(tipoProblema.getTitulo())
-				.detail(detail);
-	}
 }
--- src/main/java/com/nesrux/jmfood/api/exceptionHandler/TipoProblema.java
+++ src/main/java/com/nesrux/jmfood/api/exceptionHandler/TipoProblema.java
@@ -4,24 +4,22 @@ import lombok.Getter;
 
 @Getter
 public enum TipoProblema {
-	ENTIDADE_NAO_ENCONTRADA("entidade-nao-encontrada",
-			"Entidade não encontrada"), 
-	ENTIDADE_EM_USO("entidade-em-uso",
-					"Entidade esta em uso"),
-	ERRO_DE_NEGOCIO("violacao-negocio", 
-					"violação das regras de negócio");
+    MENSAGEM_INCOMPREENSIVEL("mensagem-incompreensivel", "Mensagem nao compreendida"),
+    ENTIDADE_NAO_ENCONTRADA("entidade-nao-encontrada", "Entidade não encontrada"),
+    ENTIDADE_EM_USO("entidade-em-uso", "Entidade esta em uso"),
+    ERRO_DE_NEGOCIO("violacao-negocio", "violação das regras de negócio");
 
-	private String titulo;
-	private String uri;
+    private String titulo;
+    private String uri;
 
-	private TipoProblema(String caminho, String titulo) {
-		this.uri = "https://jmfood.com.br/" + caminho;
-		this.titulo = titulo;
+    private TipoProblema(String caminho, String titulo) {
+	this.uri = "https://jmfood.com.br/" + caminho;
+	this.titulo = titulo;
 
-	}
+    }
 
-	/*
-	 * Enum que ajuda no builder de ErroAPI, para facilitar a escrita de novos
-	 * códigos e na manutenção do mesmo.
-	 */
+    /*
+     * Enum que ajuda no builder de ErroAPI, para facilitar a escrita de novos
+     * códigos e na manutenção do mesmo.
+     */
 }
