c53ad6cfce5655727bee4085b9f785b212e615cd
Nesrux <joaomarcosdevs@gmail.com>
2023-05-04 14:33:24 -0300

    iniciando nos testes de integração e de API

--- src/test/java/com/nesrux/jmfood/JmFoodApiApplicationTests.java
+++ src/test/java/com/nesrux/jmfood/JmFoodApiApplicationTests.java
@@ -1,13 +1,17 @@
 package com.nesrux.jmfood;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class JmFoodApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+@RunWith(SpringRunner.class)
+public class JmFoodApiApplicationTests {
+//RunWith vai rodar o código junto com o spring, 
+//Teste de intregração e teste DE api
+    @Test
+    public void contextLoads() {
+    }
 
 }
