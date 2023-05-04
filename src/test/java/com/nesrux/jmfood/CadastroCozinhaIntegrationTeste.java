e275e2d80e2a566bedb30692ad54975a58775518
Nesrux <joaomarcosdevs@gmail.com>
2023-05-04 14:50:47 -0300

    caminho infeliz e caminho feliz

---
+++ src/test/java/com/nesrux/jmfood/CadastroCozinhaIntegrationTeste.java
@@ -0,0 +1,57 @@
+package com.nesrux.jmfood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.nesrux.jmfood.domain.model.restaurante.Cozinha;
+import com.nesrux.jmfood.domain.service.CadastroCozinhaService;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CadastroCozinhaIntegrationTeste {
+//RunWith vai rodar o código junto com o spring, 
+//Teste de intregração e teste DE api
+    @Autowired
+    private CadastroCozinhaService cozinhaService;
+
+    @Test
+    public void testarCadastroComSucesso() {
+	// Caminho feliz, é o caminho onde todos os dados são validos
+
+	// ele funciona em 3 passos, o cenario onde você estancia os objetos
+	// e da um caminho para seguir.
+
+	// A ação, onde de fato o código faz algo no sistema.
+
+	// E a validação, onde o verifica se deu erro ou não.
+
+	// Cenário
+	Cozinha cozinha = new Cozinha();
+	cozinha.setNome("Italo-americana");
+
+	// Ação
+	cozinha = cozinhaService.salvar(cozinha);
+
+	// Validação
+	assertThat(cozinha).isNotNull();
+	assertThat(cozinha.getId()).isNotNull();
+    }
+
+    @Test(expected = ConstraintViolationException.class)
+    public void Testar_CadastroCozinha_SemNome() {
+	//Caminho infeliz, onde acontece algum erro
+	Cozinha cozinha = new Cozinha();
+	cozinha.setNome(null);
+	//
+	cozinha = cozinhaService.salvar(cozinha);
+
+    }
+
+}
