9de8f261766585d301b4642ace721e3fe09c0999
Nesrux <joaomarcosdevs@gmail.com>
2023-05-09 15:46:00 -0300

    atualizando o arquivo de aftermigrate

--- src/main/java/com/nesrux/jmfood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/nesrux/jmfood/core/jackson/JacksonMixinModule.java
@@ -10,20 +10,20 @@ import com.nesrux.jmfood.domain.model.endereco.Cidade;
 import com.nesrux.jmfood.domain.model.pedido.Produto;
 import com.nesrux.jmfood.domain.model.restaurante.Restaurante;
 
+/**
+ * Essa classe é um classe de config do jackson, ela é usada para fazer a
+ * "mixisagem" entre a calsse Restaurantemixin e a classe Restaurante do domain
+ * model, na hora da serialização ou descerialização do objeto ela vai utilizar
+ * as anotações que estão no mmixin e atribuir a classe que esta no model assim,
+ * a classe de modelo fica mais "limpa", e tira as anotações que só são
+ * pertinentes a API e não ao domin model como por exemplo as anotações de JPA,
+ * validation ou lombok que são pernitentes apenas à classe de dominio
+ */
+
 @Component
 public class JacksonMixinModule extends SimpleModule {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Essa classe é um classe de config do jackson, ela é usada para fazer a
-	 * "mixisagem" entre a calsse Restaurantemixin e a classe Restaurante do domain
-	 * model, na hora da serialização ou descerialização do objeto ela vai utilizar
-	 * as anotações que estão no mmixin e atribuir a classe que esta no model assim,
-	 * a classe de modelo fica mais "limpa", e tira as anotações que só são
-	 * pertinentes a API e não ao domin model como por exemplo as anotações de JPA,
-	 * validation ou lombok que são pernitentes apenas à classe de dominio
-	 */
-	
 	public JacksonMixinModule() {
 		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
 		setMixInAnnotation(Produto.class, ProdutoMixin.class);
