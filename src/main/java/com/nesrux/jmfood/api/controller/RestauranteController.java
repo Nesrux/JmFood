ac7514e69dc9984ac308bed2bce5d78ee5880948
Nesrux <joaomarcosdevs@gmail.com>
2023-05-11 15:21:29 -0300

    implementando o padrão DTO, de forma pratica

--- src/main/java/com/nesrux/jmfood/api/controller/RestauranteController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/RestauranteController.java
@@ -1,6 +1,7 @@
 package com.nesrux.jmfood.api.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nesrux.jmfood.api.model.CozinhaDTO;
+import com.nesrux.jmfood.api.model.RestauranteDTO;
 import com.nesrux.jmfood.domain.exception.NegocioException;
 import com.nesrux.jmfood.domain.exception.negocioException.EntidadeNaoEncontradaException;
 import com.nesrux.jmfood.domain.model.restaurante.Restaurante;
@@ -25,22 +28,22 @@ import com.nesrux.jmfood.domain.service.CadastroRestauranteService;
 @RequestMapping("/restaurantes")
 public class RestauranteController {
 
-
-	//private SmartValidator smartValidator; // Faz micro validacoes no bean validation
+	// private SmartValidator smartValidator; // Faz micro validacoes no bean
+	// validation
 
 	@Autowired
 	private CadastroRestauranteService restauranteService;
 
 	@GetMapping()
-	public List<Restaurante> listar() {
-		return restauranteService.acharTodos();
+	public List<RestauranteDTO> listar() {
+		return toCollectionDto(restauranteService.acharTodos());
 	}
 
 	@GetMapping("/{restauranteId}")
-	public Restaurante buscar(@PathVariable Long restauranteId) {
+	public RestauranteDTO buscar(@PathVariable Long restauranteId) {
 		Restaurante restaurante = restauranteService.acharOuFalhar(restauranteId);
 
-		return restaurante;
+		return toModel(restaurante);
 	}
 	// O @valid vai validar o objeto enviado na requisição na hora que ele chega no
 	// metodo adicionar, ao invés dele fazer isso na hora da persistencia de dados,
@@ -49,26 +52,44 @@ public class RestauranteController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Restaurante adicionar(@RequestBody @Valid Restaurante restaurante) {
+	public RestauranteDTO adicionar(@RequestBody @Valid Restaurante restaurante) {
 		try {
-			return restaurante = restauranteService.salvar(restaurante);
+			restaurante = restauranteService.salvar(restaurante);
+			return toModel(restaurante);
 		} catch (EntidadeNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage(), e);
 		}
 	}
 
 	@PutMapping("/{restauranteId}")
-	public Restaurante atualizar(@PathVariable Long restauranteId, @RequestBody @Valid Restaurante restaurante) {
+	public RestauranteDTO atualizar(@PathVariable Long restauranteId, @RequestBody @Valid Restaurante restaurante) {
 		try {
 			Restaurante restauranteAtual = restauranteService.acharOuFalhar(restauranteId);
 
 			BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco",
 					"dataCadastro");
 
-			return restauranteService.salvar(restauranteAtual);
+			return toModel(restauranteService.salvar(restauranteAtual));
 		} catch (EntidadeNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage(), e);
 		}
+	}
+
+	private List<RestauranteDTO> toCollectionDto(List<Restaurante> restaurantes) {
+		return restaurantes.stream().map(restaurante -> toModel(restaurante)).collect(Collectors.toList());
+	}
+
+	private RestauranteDTO toModel(Restaurante restaurante) {
+		CozinhaDTO cozinhaDTO = new CozinhaDTO();
+		cozinhaDTO.setId(restaurante.getCozinha().getId());
+		cozinhaDTO.setNome(restaurante.getCozinha().getNome());
+
+		RestauranteDTO restauranteDTO = new RestauranteDTO();
+		restauranteDTO.setId(restaurante.getId());
+		restauranteDTO.setNome(restaurante.getNome());
+		restauranteDTO.setTaxaFrete(restaurante.getTaxaFrete());
+		restauranteDTO.setCozinha(cozinhaDTO);
+		return restauranteDTO;
 	}
 
 }
