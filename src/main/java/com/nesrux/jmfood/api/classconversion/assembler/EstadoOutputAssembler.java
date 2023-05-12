31f88fcc5220ebf6405e98b13a05648c18731158
Nesrux <joaomarcosdevs@gmail.com>
2023-05-12 18:23:46 -0300

    estado output Assembler finalizado

--- src/main/java/com/nesrux/jmfood/api/classconversion/assembler/EstadoOutputAssembler.java
+++ src/main/java/com/nesrux/jmfood/api/classconversion/assembler/EstadoOutputAssembler.java
@@ -1,17 +1,23 @@
 package com.nesrux.jmfood.api.classconversion.assembler;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.nesrux.jmfood.api.model.dto.output.estado.EstadoOutputDto;
+import com.nesrux.jmfood.domain.model.endereco.Estado;
 
 public class EstadoOutputAssembler {
-	
+
 	@Autowired
 	private ModelMapper mapper;
-	
-	public EstadoOutputDto toModel() {
-		return null;
+
+	public EstadoOutputDto toModel(Estado estado) {
+		return mapper.map(estado, EstadoOutputDto.class);
 	}
 
+	public List<EstadoOutputDto> toCollectionDto(List<Estado> estados) {
+		return estados.stream().map(estado -> mapper.map(estados, EstadoOutputDto.class)).toList();
+	}
 }
--- src/main/java/com/nesrux/jmfood/api/model/dto/output/estado/EstadoOutputDto.java
+++ src/main/java/com/nesrux/jmfood/api/model/dto/output/estado/EstadoOutputDto.java
@@ -1,5 +1,13 @@
 package com.nesrux.jmfood.api.model.dto.output.estado;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EstadoOutputDto {
+
+	private Long id;
+	private String nome;
 
 }
