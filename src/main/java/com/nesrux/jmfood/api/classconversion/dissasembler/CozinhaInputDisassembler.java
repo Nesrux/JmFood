87252c12408b7970161a8666a593d84851761b19
Nesrux <joaomarcosdevs@gmail.com>
2023-05-12 20:00:48 -0300

    reafotando nome da classe

--- src/main/java/com/nesrux/jmfood/api/classconversion/dissasembler/CozinhaInputDisassembler.java
+++ src/main/java/com/nesrux/jmfood/api/classconversion/dissasembler/CozinhaInputDisassembler.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EstadoOutputDisassembler {
+public class CozinhaInputDisassembler {
 	@Autowired
 	private ModelMapper modelMapper;
 	 
