a01724503086852a5deb2905c00b309aa3051df8
Nesrux <joaomarcosdevs@gmail.com>
2023-05-13 11:25:28 -0300

    ]adicionando classe de DTO de Cidade

---
+++ src/main/java/com/nesrux/jmfood/api/model/dto/input/cidade/CidadeInputDto.java
@@ -0,0 +1,5 @@
+package com.nesrux.jmfood.api.model.dto.input.cidade;
+
+public class CidadeInputDto {
+
+}
---
+++ src/main/java/com/nesrux/jmfood/api/model/dto/output/cidade/CidadeOutputDto.java
@@ -0,0 +1,5 @@
+package com.nesrux.jmfood.api.model.dto.output.cidade;
+
+public class CidadeOutputDto {
+
+}
