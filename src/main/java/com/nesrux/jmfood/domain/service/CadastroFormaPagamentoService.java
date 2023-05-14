19c2bd198bc6c83f152bfecba805ee6f9dfa2d6b
Nesrux <joaomarcosdevs@gmail.com>
2023-05-14 17:40:31 -0300

    service de formaPagamento completo

--- src/main/java/com/nesrux/jmfood/domain/service/CadastroFormaPagamentoService.java
+++ src/main/java/com/nesrux/jmfood/domain/service/CadastroFormaPagamentoService.java
@@ -2,15 +2,22 @@ package com.nesrux.jmfood.domain.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
+import com.nesrux.jmfood.domain.exception.negocioException.EntidadeEmUsoException;
 import com.nesrux.jmfood.domain.exception.negocioException.entidadeNaoEncontrada.FormaPagamentoNaoEncontradaException;
 import com.nesrux.jmfood.domain.model.pedido.FormaPagamento;
 import com.nesrux.jmfood.domain.repository.FormaPagamentoRepository;
 
 @Component
 public class CadastroFormaPagamentoService {
+
+	private static final String MSG_FORMAPAGAMENTO_EM_USO = "Forma de pagamento de código %d não pode ser removida, pois está em uso";
 	@Autowired
 	private FormaPagamentoRepository repository;
 
@@ -18,16 +25,26 @@ public class CadastroFormaPagamentoService {
 		return repository.findById(formaPagamentoId)
 				.orElseThrow(() -> new FormaPagamentoNaoEncontradaException(formaPagamentoId));
 	}
-	
-	public List<FormaPagamento> acharTodos(){
-		return repository.findAll();
-	}	
 
-	
-	public FormaPagamento salvar(FormaPagamento formaPagamento) {
-		
-		
+	public List<FormaPagamento> acharTodos() {
+		return repository.findAll();
 	}
-	public void excluir () {}
-	
+
+	@Transactional
+	public FormaPagamento salvar(FormaPagamento formaPagamento) {
+		return repository.save(formaPagamento);
+	}
+
+	@Transactional
+	public void excluir(Long formaPagamentoId) {
+		try {
+			repository.deleteById(formaPagamentoId);
+			repository.flush();
+		} catch (EmptyResultDataAccessException e) {
+			throw new FormaPagamentoNaoEncontradaException(formaPagamentoId);
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(MSG_FORMAPAGAMENTO_EM_USO);
+		}
+	}
+
 }
