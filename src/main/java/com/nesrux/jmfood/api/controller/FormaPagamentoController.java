964484c25742ad3de9015ae5041832223d544beb
Nesrux <joaomarcosdevs@gmail.com>
2023-05-14 18:21:39 -0300

    Crud de forma-pagamento completo

--- src/main/java/com/nesrux/jmfood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/FormaPagamentoController.java
@@ -2,14 +2,22 @@ package com.nesrux.jmfood.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nesrux.jmfood.api.classconversion.assembler.FormaPagamentoOutputAssembler;
 import com.nesrux.jmfood.api.classconversion.dissasembler.FormaPagamentoInputDisassembler;
+import com.nesrux.jmfood.api.model.dto.input.formaPagamento.FormaPagamentoInputDto;
 import com.nesrux.jmfood.api.model.dto.output.formaPagamento.FormaPagamentoOutputDto;
 import com.nesrux.jmfood.domain.model.pedido.FormaPagamento;
 import com.nesrux.jmfood.domain.service.CadastroFormaPagamentoService;
@@ -37,6 +45,22 @@ public class FormaPagamentoController {
 		FormaPagamento formaPagamento = service.acharOuFalhar(formaPagamentoID);
 		
 		return assembler.toModel(formaPagamento);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public FormaPagamentoOutputDto adicionar(@RequestBody @Valid FormaPagamentoInputDto inputDto) {
+		FormaPagamento formaPagamento = disassembler.toDomainObject(inputDto);
+		
+		FormaPagamentoOutputDto outputDto = assembler.toModel(service.salvar(formaPagamento));
+		
+		return outputDto;
+	}
+	
+	@DeleteMapping("/{formaPagamentoID}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void excluir (@PathVariable Long formaPagamentoID) {
+		service.excluir(formaPagamentoID);
 	}
 
 }
--- src/main/java/com/nesrux/jmfood/api/model/dto/input/formaPagamento/FormaPagamentoInputDto.java
+++ src/main/java/com/nesrux/jmfood/api/model/dto/input/formaPagamento/FormaPagamentoInputDto.java
@@ -1,12 +1,13 @@
 package com.nesrux.jmfood.api.model.dto.input.formaPagamento;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class FormaPagamentoInputDto {
-	
+	@NotBlank
 	private String descricao;
-
 }
