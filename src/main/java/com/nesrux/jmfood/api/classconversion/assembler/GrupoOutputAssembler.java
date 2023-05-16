d7c079c3ae6c210ab9d686d5d5d1d6f21128e81f
Nesrux <joaomarcosdevs@gmail.com>
2023-05-16 17:06:41 -0300

    criando a classe de Assembler de grupos

--- src/main/java/com/nesrux/jmfood/api/classconversion/assembler/GrupoOutputAssembler.java
+++ src/main/java/com/nesrux/jmfood/api/classconversion/assembler/GrupoOutputAssembler.java
@@ -1,13 +1,28 @@
 package com.nesrux.jmfood.api.classconversion.assembler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.nesrux.jmfood.domain.service.CadastroGrupoService;
+import com.nesrux.jmfood.api.model.dto.output.grupo.GrupoModel;
+import com.nesrux.jmfood.domain.model.user.Grupo;
 
 @Component
 public class GrupoOutputAssembler {
 	@Autowired
-	private CadastroGrupoService service;
+	private ModelMapper mapper;
 
+	public GrupoModel toModel(Grupo grupo) {
+		return mapper.map(grupo,GrupoModel.class);
+	}
+	
+	public List<GrupoModel> toCollectionDto(List<Grupo> grupos){
+		return grupos.stream()
+				.map(grupo -> toModel(grupo)).collect(Collectors.toList());
+		}
+	
+	
 }
