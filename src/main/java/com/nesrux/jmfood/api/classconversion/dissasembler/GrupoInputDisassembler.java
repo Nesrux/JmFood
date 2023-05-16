1feefc9920ea1c134e86a82a3c92cb1293da7397
Nesrux <joaomarcosdevs@gmail.com>
2023-05-16 17:09:57 -0300

    GrupoinputDissassembler completo

---
+++ src/main/java/com/nesrux/jmfood/api/classconversion/dissasembler/GrupoInputDisassembler.java
@@ -0,0 +1,23 @@
+package com.nesrux.jmfood.api.classconversion.dissasembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.nesrux.jmfood.api.model.dto.input.grupo.GrupoInputDto;
+import com.nesrux.jmfood.domain.model.user.Grupo;
+
+@Component
+public class GrupoInputDisassembler {
+	@Autowired
+	private ModelMapper mapper;
+
+	public Grupo toDomainObject(GrupoInputDto inputDto) {
+		return mapper.map(inputDto, Grupo.class);
+	}
+
+	public void copyToDomainObject(GrupoInputDto inputDto, Grupo grupo) {
+		mapper.map(inputDto, grupo);
+	}
+
+}
