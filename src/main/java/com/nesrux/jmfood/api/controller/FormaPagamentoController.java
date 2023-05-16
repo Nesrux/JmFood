a43548624d16d7b858e7f69832172b7e2e665187
Nesrux <joaomarcosdevs@gmail.com>
2023-05-16 18:03:33 -0300

    adicionando o PUT de formaPagamento

--- src/main/java/com/nesrux/jmfood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/FormaPagamentoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -39,27 +40,37 @@ public class FormaPagamentoController {
 	public List<FormaPagamentoModel> listar() {
 		return assembler.toCollectionDto(service.acharTodos());
 	}
-	
+
 	@GetMapping("/{formaPagamentoID}")
 	public FormaPagamentoModel buscar(@PathVariable Long formaPagamentoID) {
 		FormaPagamento formaPagamento = service.acharOuFalhar(formaPagamentoID);
-		
+
 		return assembler.toModel(formaPagamento);
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public FormaPagamentoModel adicionar(@RequestBody @Valid FormaPagamentoInputDto inputDto) {
 		FormaPagamento formaPagamento = disassembler.toDomainObject(inputDto);
-		
+
 		FormaPagamentoModel outputDto = assembler.toModel(service.salvar(formaPagamento));
-		
+
 		return outputDto;
 	}
-	
+
+	@PutMapping("/{formaPagamentoId}")
+	public FormaPagamentoModel atualizar(@RequestBody FormaPagamentoInputDto inputDto,
+			@PathVariable Long formaPagamentoId) {
+		FormaPagamento formaPagamento = service.acharOuFalhar(formaPagamentoId);
+		disassembler.copyToDomainObject(inputDto, formaPagamento);
+		service.salvar(formaPagamento);
+
+		return assembler.toModel(formaPagamento);
+	}
+
 	@DeleteMapping("/{formaPagamentoID}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void excluir (@PathVariable Long formaPagamentoID) {
+	public void excluir(@PathVariable Long formaPagamentoID) {
 		service.excluir(formaPagamentoID);
 	}
 
