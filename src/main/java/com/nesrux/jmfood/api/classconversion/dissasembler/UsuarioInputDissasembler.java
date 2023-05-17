8c74bebcd5b8b70f2224531f07d80c91ccf3c904
Nesrux <joaomarcosdevs@gmail.com>
2023-05-17 14:45:03 -0300

    classe dissassembler de usuario completo

---
+++ src/main/java/com/nesrux/jmfood/api/classconversion/dissasembler/UsuarioInputDissasembler.java
@@ -0,0 +1,23 @@
+package com.nesrux.jmfood.api.classconversion.dissasembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.nesrux.jmfood.api.model.dto.input.usuario.UsuarioInput;
+import com.nesrux.jmfood.domain.model.user.Usuario;
+
+@Component
+public class UsuarioInputDissasembler {
+	@Autowired
+	private ModelMapper mapper;
+
+	public Usuario toDomainObject(UsuarioInput input) {
+		return mapper.map(input, Usuario.class);
+	}
+
+	public void copyToDomainObject(UsuarioInput input, Usuario usuario) {
+		mapper.map(input, usuario);
+	}
+
+}
