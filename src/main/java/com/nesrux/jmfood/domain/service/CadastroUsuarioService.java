d46ea6070a8c8cce13a7dd967c33f0cf9960754e
Nesrux <joaomarcosdevs@gmail.com>
2023-05-17 13:43:15 -0300

    adicionando métodos ao service de usuario

--- src/main/java/com/nesrux/jmfood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/nesrux/jmfood/domain/service/CadastroUsuarioService.java
@@ -1,5 +1,37 @@
 package com.nesrux.jmfood.domain.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.nesrux.jmfood.domain.exception.negocioException.entidadeNaoEncontrada.UsuarioNaoEncontradoException;
+import com.nesrux.jmfood.domain.model.user.Usuario;
+import com.nesrux.jmfood.domain.repository.UsuarioRepository;
+
+@Component
 public class CadastroUsuarioService {
+	@Autowired
+	private UsuarioRepository repository;
+
+	public List<Usuario> acharTodos() {
+		return repository.findAll();
+	}
+
+	public Usuario acharOuFalhar(Long usuarioId) {
+		return repository.findById(usuarioId).orElseThrow(() -> new UsuarioNaoEncontradoException(usuarioId));
+	}
+
+	public Usuario Salvar(Usuario usuario) {
+		return repository.save(usuario);
+	}
+
+	public String acharEmail(Long usuarioId) {
+		return repository.findEmailByID(usuarioId);
+	}
+
+	public String acharSenha(Long usuarioId) {
+		return repository.findEmailByID(usuarioId);
+	}
 
 }
