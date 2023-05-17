fa8c64f4ea2d03256feb22270a61208221927aee
Nesrux <joaomarcosdevs@gmail.com>
2023-05-17 13:36:27 -0300

    criando a exception usuario não encontrado

---
+++ src/main/java/com/nesrux/jmfood/domain/exception/negocioException/entidadeNaoEncontrada/UsuarioNaoEncontradoException.java
@@ -0,0 +1,17 @@
+package com.nesrux.jmfood.domain.exception.negocioException.entidadeNaoEncontrada;
+
+import com.nesrux.jmfood.domain.exception.negocioException.EntidadeNaoEncontradaException;
+
+public class UsuarioNaoEncontradoException extends EntidadeNaoEncontradaException {
+
+	private static final long serialVersionUID = 1L;
+
+	public UsuarioNaoEncontradoException(String mensagem) {
+		super(mensagem);
+	}
+
+	public UsuarioNaoEncontradoException(Long userId) {
+		this(String.format("O usuario de id %d não existe", userId));
+	}
+
+}
