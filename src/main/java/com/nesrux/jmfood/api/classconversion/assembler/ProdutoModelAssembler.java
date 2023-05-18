8c3024cd18b56dfc5fa54c8d790ee8f4999c5437
Nesrux <joaomarcosdevs@gmail.com>
2023-05-18 15:24:11 -0300

    criando a classe de assembler de produtoModel

---
+++ src/main/java/com/nesrux/jmfood/api/classconversion/assembler/ProdutoModelAssembler.java
@@ -0,0 +1,26 @@
+package com.nesrux.jmfood.api.classconversion.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.nesrux.jmfood.api.model.dto.output.produto.ProdutoModel;
+import com.nesrux.jmfood.domain.model.pedido.Produto;
+
+@Component
+public class ProdutoModelAssembler {
+	@Autowired
+	private ModelMapper mapper;
+
+	public ProdutoModel toModel(Produto produto) {
+		return mapper.map(produto, ProdutoModel.class);
+	}
+
+	public List<ProdutoModel> toCollectionDto(List<Produto> produtos) {
+		return produtos.stream().map(produto -> toModel(produto)).collect(Collectors.toList());
+	}
+
+}
