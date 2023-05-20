8ac0e02497b43d98bac451699ace56490f077b2c
Nesrux <joaomarcosdevs@gmail.com>
2023-05-20 14:36:18 -0300

    criando controller de pedido

---
+++ src/main/java/com/nesrux/jmfood/api/controller/PedidoController.java
@@ -0,0 +1,35 @@
+package com.nesrux.jmfood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nesrux.jmfood.api.classconversion.assembler.PedidoModelAssembler;
+import com.nesrux.jmfood.api.model.dto.output.pedido.PedidoModel;
+import com.nesrux.jmfood.domain.model.pedido.Pedido;
+import com.nesrux.jmfood.domain.service.CadastroPedidoService;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+	@Autowired
+	private CadastroPedidoService service;
+	@Autowired
+	private PedidoModelAssembler assembler;
+
+	@GetMapping
+	public List<PedidoModel> listarPedidos() {
+		return assembler.toCollectionDto(service.Listar());
+	}
+
+	@GetMapping("/{pedidoId}")
+	public PedidoModel buscar(@PathVariable Long pedidoId) {
+		Pedido pedido = service.acharOuFalhar(pedidoId);
+		return assembler.toModel(pedido);
+	}
+
+}
