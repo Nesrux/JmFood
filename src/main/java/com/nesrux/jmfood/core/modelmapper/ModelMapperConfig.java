597c8b5e0ed4b7a7045653cf8927da3dca2f9216
Nesrux <joaomarcosdevs@gmail.com>
2023-05-20 16:46:27 -0300

    resolvendo o bug de retornar Null na representação da classe Pedido model no atributo dataCriacao

--- src/main/java/com/nesrux/jmfood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/nesrux/jmfood/core/modelmapper/ModelMapperConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.nesrux.jmfood.api.model.dto.output.endereco.EnderecoModel;
+import com.nesrux.jmfood.api.model.dto.output.pedido.PedidoModel;
 import com.nesrux.jmfood.domain.model.endereco.Endereco;
+import com.nesrux.jmfood.domain.model.pedido.Pedido;
 
 @Configuration
 public class ModelMapperConfig {
@@ -24,7 +26,10 @@ public class ModelMapperConfig {
 		// .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setTaxaFrete);
 
 		var enderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
-	
+		var dateTimePedidoParaPedidoModel = modelMapper.createTypeMap(Pedido.class, PedidoModel.class);
+		
+		dateTimePedidoParaPedidoModel.addMapping(Pedido::getDataCriacao, PedidoModel::setDataCricao); //Por algum motivo nao tava estava indo automaticamente
+		
 		enderecoModelTypeMap.<String>addMapping(src -> src.getCidade().getEstado().getNome(),
 				(dest, value) -> dest.getCidade().setEstado(value));
 	
