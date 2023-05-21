f493806e68d8e3ec62698c74c97e0e79772f6eaf
Nesrux <joaomarcosdevs@gmail.com>
2023-05-21 14:28:26 -0300

    criando método de PUT em /pedidos

--- src/main/java/com/nesrux/jmfood/api/controller/PedidoController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/PedidoController.java
@@ -3,13 +3,18 @@ package com.nesrux.jmfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nesrux.jmfood.api.classconversion.assembler.PedidoModelAssembler;
 import com.nesrux.jmfood.api.classconversion.assembler.PedidoResumoModelAssembler;
+import com.nesrux.jmfood.api.classconversion.dissasembler.PedidoInputDisasselber;
+import com.nesrux.jmfood.api.model.dto.input.pedido.PedidoInputDto;
 import com.nesrux.jmfood.api.model.dto.output.pedido.PedidoModel;
 import com.nesrux.jmfood.api.model.dto.output.pedido.PedidoResumoModel;
 import com.nesrux.jmfood.domain.model.pedido.Pedido;
@@ -20,10 +25,15 @@ import com.nesrux.jmfood.domain.service.CadastroPedidoService;
 public class PedidoController {
 	@Autowired
 	private CadastroPedidoService service;
+
 	@Autowired
 	private PedidoModelAssembler assembler;
+
 	@Autowired
 	private PedidoResumoModelAssembler resumoAssembler;
+
+	@Autowired
+	private PedidoInputDisasselber pedidoDisasselber;
 
 	@GetMapping
 	public List<PedidoResumoModel> listarPedidos() {
@@ -34,6 +44,14 @@ public class PedidoController {
 	public PedidoModel buscar(@PathVariable Long pedidoId) {
 		Pedido pedido = service.acharOuFalhar(pedidoId);
 		return assembler.toModel(pedido);
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public PedidoModel salvar(PedidoInputDto inputDto) {
+		Pedido pedido = pedidoDisasselber.toDomainObject(inputDto);
+
+		return assembler.toModel(service.salvar(pedido));
 	}
 
 }
