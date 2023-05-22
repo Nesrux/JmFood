0ae3a24862703c528f5fb24c542a8741f0a7acef
Nesrux <joaomarcosdevs@gmail.com>
2023-05-22 18:18:11 -0300

    criando o controller de Fluxo Pedido

---
+++ src/main/java/com/nesrux/jmfood/api/controller/subcontrollers/pedidos/FluxoPedidoController.java
@@ -0,0 +1,36 @@
+package com.nesrux.jmfood.api.controller.subcontrollers.pedidos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nesrux.jmfood.domain.service.FluxoPedidoService;
+
+@RestController
+@RequestMapping("/pedidos/{pedidoId}")
+public class FluxoPedidoController {
+	@Autowired
+	private FluxoPedidoService fluxoService;
+
+	@PutMapping("/confirmar")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void confirmarPedido(@PathVariable Long PedidoId) {
+		fluxoService.confirmar(PedidoId);
+	}
+
+	@PutMapping("/entregar")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void entregarPedido(@PathVariable Long PedidoId) {
+		fluxoService.confirmar(PedidoId);
+	}
+
+	@PutMapping("/cancelar")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void cancelarrPedido(@PathVariable Long PedidoId) {
+		fluxoService.confirmar(PedidoId);
+	}
+}
--- src/main/java/com/nesrux/jmfood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/nesrux/jmfood/domain/service/FluxoPedidoService.java
@@ -1,8 +1,31 @@
 package com.nesrux.jmfood.domain.service;
 
+import java.time.OffsetDateTime;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.nesrux.jmfood.domain.exception.NegocioException;
+import com.nesrux.jmfood.domain.model.pedido.Pedido;
+import com.nesrux.jmfood.domain.model.pedido.StatusPedido;
 
 @Service
 public class FluxoPedidoService {
+	@Autowired
+	private CadastroPedidoService pedidoService;
+
+	@Transactional
+	public void confirmar(Long pedidoId) {
+		Pedido pedido = pedidoService.acharOuFalhar(pedidoId);
+
+		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+			throw new NegocioException(String.format("Status do pedido %d não pode ser alterado de %s para %s",
+					pedidoId, pedido.getStatus(), StatusPedido.CONFIRMADO));
+		}
+		pedido.setStatus(StatusPedido.CONFIRMADO);
+		pedido.setDataConfirmacao(OffsetDateTime.now());
+	}
 
 }
