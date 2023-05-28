df9963288fdc3c183e1eacc30af444858bef3613
Nesrux <joaomarcosdevs@gmail.com>
2023-05-28 18:24:41 -0300

    criando um endpoint que retorna PDF em controller/estatisticas

--- src/main/java/com/nesrux/jmfood/api/controller/EstatisticasController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/EstatisticasController.java
@@ -3,6 +3,7 @@ package com.nesrux.jmfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nesrux.jmfood.domain.filter.VendaDiariaFilter;
 import com.nesrux.jmfood.domain.model.dto.VendaDiaria;
 import com.nesrux.jmfood.domain.service.VendaQueryService;
+import com.nesrux.jmfood.domain.service.VendaReportService;
 
 @RestController
 @RequestMapping("/estatisticas")
 public class EstatisticasController {
 	@Autowired
 	private VendaQueryService vendaQueryService;
+	
+	@Autowired
+	private VendaReportService reportService;
 
 	@GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,
@@ -29,7 +34,16 @@ public class EstatisticasController {
 	@GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
 	public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
 			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
-		return null;
+
+		byte[] bytesPdf = reportService.emitirVendasDiarias(filtro, timeOffset);
+		
+		var header = new HttpHeaders();
+		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=elmaMariaPinto.pdf");//HAHA comédias
+		
+		return ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_PDF)
+				.headers(header)
+				.body(bytesPdf);
 	}
 
 
