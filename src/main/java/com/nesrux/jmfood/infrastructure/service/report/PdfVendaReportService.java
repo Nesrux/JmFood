26185e5fdb56b03bbafd2579718e643b405bb4db
Nesrux <joaomarcosdevs@gmail.com>
2023-05-28 21:16:45 -0300

    criando um método que implementa o envio de Pdf

--- src/main/java/com/nesrux/jmfood/infrastructure/service/report/PdfVendaReportService.java
+++ src/main/java/com/nesrux/jmfood/infrastructure/service/report/PdfVendaReportService.java
@@ -1,16 +1,42 @@
 package com.nesrux.jmfood.infrastructure.service.report;
 
+import java.util.HashMap;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nesrux.jmfood.domain.exception.NegocioException;
 import com.nesrux.jmfood.domain.filter.VendaDiariaFilter;
+import com.nesrux.jmfood.domain.service.VendaQueryService;
 import com.nesrux.jmfood.domain.service.VendaReportService;
 
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
 @Service
-public class PdfVendaReportService implements VendaReportService{
+public class PdfVendaReportService implements VendaReportService {
+
+	@Autowired
+	private VendaQueryService vendaService;
 
 	@Override
-	public byte[] emitirVendasDiarias(VendaDiariaFilter filtar, String timeOffset) {
-		return null;
+	public byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
+		try {
+			var inputStream = this.getClass().getResourceAsStream("/relatorios/jmfoodport.jasper");
+
+			var params = new HashMap<String, Object>();
+			params.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+			var vendasDiarias = vendaService.consultarVendaDiarias(filtro, timeOffset);
+			var dataSource = new JRBeanCollectionDataSource(vendasDiarias);
+
+			var jasperPrint = JasperFillManager.fillReport(inputStream, params, dataSource);
+			return JasperExportManager.exportReportToPdf(jasperPrint);
+		} catch (Exception e) {
+			throw new NegocioException("deu ruim");
+		}
 	}
 
 }
