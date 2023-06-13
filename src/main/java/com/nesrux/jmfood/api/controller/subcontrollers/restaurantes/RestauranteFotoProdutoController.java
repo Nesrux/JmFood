a9ee84584e41c69ba319bf2ede3ca91441bd06a2
Nesrux <joaomarcosdevs@gmail.com>
2023-06-13 15:30:35 -0300

    adicionando negociação de conteúdo para o endpoint de fotos

--- src/main/java/com/nesrux/jmfood/api/controller/subcontrollers/restaurantes/RestauranteFotoProdutoController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/subcontrollers/restaurantes/RestauranteFotoProdutoController.java
@@ -2,6 +2,7 @@ package com.nesrux.jmfood.api.controller.subcontrollers.restaurantes;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -10,9 +11,11 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -65,19 +68,33 @@ public class RestauranteFotoProdutoController {
 		return assembler.toModel(fotoProduto);
 	}
 
-	@GetMapping(produces = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_JPEG_VALUE })
+	@GetMapping
 	public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId,
-			@PathVariable Long produtoId) {
+			@PathVariable Long produtoId, @RequestHeader(name = "accept") String acceptHeader)
+			throws HttpMediaTypeNotAcceptableException {
 		try {
 			FotoProduto fotoProduto = service.buscarOuFalhar(restauranteId, produtoId);
+			MediaType mediaTypeFoto = MediaType.parseMediaType(fotoProduto.getContentType());
+			List<MediaType> mediaTypeAceitas = MediaType.parseMediaTypes(acceptHeader);
+
+			verificarMediaTypeFoto(mediaTypeFoto, mediaTypeAceitas);
+
 			InputStream inputStram = fotoStorage.recuperar(fotoProduto.getNome());
 
-			return ResponseEntity.ok()
-					.contentType(MediaType.IMAGE_JPEG)
-					.body(new InputStreamResource(inputStram));
-			
+			return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(new InputStreamResource(inputStram));
+
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+
+	private void verificarMediaTypeFoto(MediaType mediaTypeFoto, List<MediaType> mediaTypeAceitas)
+			throws HttpMediaTypeNotAcceptableException {
+		boolean compativel = mediaTypeAceitas.stream()
+				.anyMatch(mediaTypeAceita -> mediaTypeAceita.isCompatibleWith(mediaTypeFoto));
+
+		if (!compativel) {
+			throw new HttpMediaTypeNotAcceptableException(mediaTypeAceitas);
 		}
 	}
 
