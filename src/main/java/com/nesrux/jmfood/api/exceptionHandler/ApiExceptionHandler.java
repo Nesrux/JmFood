9f9829214386ba58a0fe337335c6aed2a34fe0a8
Nesrux <joaomarcosdevs@gmail.com>
2023-06-14 18:40:03 -0300

    sobrescrevendo o handle HttpnottypeAcceptable

--- src/main/java/com/nesrux/jmfood/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/nesrux/jmfood/api/exceptionHandler/ApiExceptionHandler.java
@@ -16,6 +16,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -219,6 +220,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 			WebRequest request) {
 
 		return handleValidationInternal(ex, headers, status, request, ex.getBindingResult());
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return ResponseEntity.status(status).headers(headers).build();
 	}
 
 	// Trata A exeception de propriedade inválida no corpo da requisição
