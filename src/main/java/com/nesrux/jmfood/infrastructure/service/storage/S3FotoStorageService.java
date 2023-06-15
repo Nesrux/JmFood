9ba3ddcf861a33da31bec7fc3769b427bc6cffb9
Nesrux <joaomarcosdevs@gmail.com>
2023-06-15 13:27:37 -0300

    configurando o AWS s3

--- src/main/java/com/nesrux/jmfood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/nesrux/jmfood/infrastructure/service/storage/S3FotoStorageService.java
@@ -2,12 +2,17 @@ package com.nesrux.jmfood.infrastructure.service.storage;
 
 import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.nesrux.jmfood.domain.service.FotoStorageService;
 
 @Service
 public class S3FotoStorageService implements FotoStorageService {
+	
+	@Autowired
+	private AmazonS3 amazonS3;
 
 	@Override
 	public void armazenar(NovaFoto novafoto) {
