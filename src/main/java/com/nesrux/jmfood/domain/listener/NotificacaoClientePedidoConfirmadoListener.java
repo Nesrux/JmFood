4ff7d663b35eb105094b3b7d4620dac3d74c2470
Nesrux <joaomarcosdevs@gmail.com>
2023-06-19 12:29:19 -0300

    resolvendo bug de envio de email quando confirmado

--- src/main/java/com/nesrux/jmfood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/nesrux/jmfood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-import com.nesrux.jmfood.domain.event.PedidoCanceladoEvent;
+import com.nesrux.jmfood.domain.event.PedidoConfirmadoEvent;
 import com.nesrux.jmfood.domain.model.pedido.Pedido;
 import com.nesrux.jmfood.domain.service.EnvioEmailService;
 import com.nesrux.jmfood.domain.service.EnvioEmailService.Mensagem;
@@ -15,16 +15,14 @@ public class NotificacaoClientePedidoConfirmadoListener {
 	private EnvioEmailService emailService;
 
 	@TransactionalEventListener
-	public void quandoConfirmarPedido(PedidoCanceladoEvent event) {
+	public void quandoConfirmarPedido(PedidoConfirmadoEvent event) {
 		Pedido pedido = event.getPedido();
 		var mensagem = Mensagem.builder()
-				.assunto(pedido.getRestaurante()
-				.getNome() + " - pedido confirmado")
+				.assunto(pedido.getRestaurante().getNome() + " - pedido confirmado")
 				.corpo("pedidoConfirmado.html")
 				.variavel("pedido", pedido)
 				.destinatario(pedido.getCliente().getEmail())
 				.build();
-
 		emailService.enviar(mensagem);
 
 	}
