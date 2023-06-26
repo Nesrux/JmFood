751a537ac00cf5ff40baac222548a92be39de977
Nesrux <joaomarcosdevs@gmail.com>
2023-06-26 15:59:41 -0300

    removendo a pasta /bin do java

--- bin/src/main/resources/db/migration/V010__cria-tabela-restaurante_forma_pagamento.sql
+++
@@ -1,6 +0,0 @@
-create table restaurante_forma_pagamento (
-	restaurante_id bigint not null,
-	forma_pagamento_id bigint not null,
-	
-	primary key (restaurante_id, forma_pagamento_id)
-) engine=InnoDB default charset=utf8;
--- bin/src/main/resources/db/migration/V011__cria-tabela-usuario.sql
+++
@@ -1,9 +0,0 @@
-create table usuario (
-	id bigint not null auto_increment,
-	nome varchar(80) not null,
-	email varchar(255) not null,
-	senha varchar(255) not null,
-	data_cadastro datetime not null,
-	
-	primary key (id)
-) engine=InnoDB default charset=utf8;
