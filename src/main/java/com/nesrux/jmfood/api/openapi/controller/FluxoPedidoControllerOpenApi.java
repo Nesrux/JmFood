e85f74b5bb7cd134292eb1fc6c966e0071ac4c2b
Nesrux <joaomarcosdevs@gmail.com>
2023-06-27 19:24:12 -0300

    desafio do mapeamento dos coiso la

--- src/main/java/com/nesrux/jmfood/api/openapi/controller/FluxoPedidoControllerOpenApi.java
+++ src/main/java/com/nesrux/jmfood/api/openapi/controller/FluxoPedidoControllerOpenApi.java
@@ -2,17 +2,21 @@ package com.nesrux.jmfood.api.openapi.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @Api(tags = "Pedidos")
 public interface FluxoPedidoControllerOpenApi {
 
 	@ApiOperation("Confirmação de um pedido")
-	public void confirmarPedido(String codigoPedido);
+	public void confirmarPedido(
+			@ApiParam(value = "codigo de um pedido", example = "d178b637-a785-4768-a3cb-aa1ce5a8cdab", required = true) String codigoPedido);
 
 	@ApiOperation("entrega de um pedido")
-	public void entregarPedido(String codigoPedido);
+	public void entregarPedido(
+			@ApiParam(value = "codigo de um pedido", example = "d178b637-a785-4768-a3cb-aa1ce5a8cdab", required = true) String codigoPedido);
 
 	@ApiOperation("Cancelamento de um pedido")
-	public void cancelarPedido(String codigoPedido);
+	public void cancelarPedido(
+			@ApiParam(value = "codigo de um pedido", example = "d178b637-a785-4768-a3cb-aa1ce5a8cdab", required = true) String codigoPedido);
 
 }
