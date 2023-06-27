6827b698f24704ae40220153e1ac8e41a97bc0f5
Nesrux <joaomarcosdevs@gmail.com>
2023-06-27 16:43:51 -0300

    desafio do completo

--- src/main/java/com/nesrux/jmfood/api/openapi/controller/RestauranteControllerOpenApi.java
+++ src/main/java/com/nesrux/jmfood/api/openapi/controller/RestauranteControllerOpenApi.java
@@ -11,6 +11,8 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Api(tags = "Restaurantes")
 public interface RestauranteControllerOpenApi {
@@ -22,12 +24,21 @@ public interface RestauranteControllerOpenApi {
 					name = "projeção",
 					paramType = "query",
 					type = "string") })
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "A listagem de pedidos foi feito com sucesso")
+	})
 	public List<RestauranteModel> listarResumo();
 
 	@ApiOperation(value = "Listagem de restaurantes", hidden = true)
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "A listagem de pedidos foi feito com sucesso")
+	})
 	public List<RestauranteModel> listarNomes();
 	
 	@ApiOperation("Busca de um unico restaurante")
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Não foi possivel encontrar um restaurante com o id passado")
+	})
 	public RestauranteModel buscar(@ApiParam(value = "Id de um restaurante", example = "1", required = true) Long restauranteId);
 	
 	@ApiOperation("Cadastro de um restaurante")
@@ -35,25 +46,52 @@ public interface RestauranteControllerOpenApi {
 		RestauranteInputDto restauranteInputDTO);
 	
 	@ApiOperation("Atualização de um restaurante")
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Não foi possivel encontrar um restaurante com o id passado")
+	})
 	public RestauranteModel atualizar(@ApiParam(value = "Id de um restaurante", example = "1", required = true)Long restauranteId,
 			@ApiParam(name = "corpo", value = "Representação de um restaurante", required = true) RestauranteInputDto restauranteInputDto);
 	
 	@ApiOperation("Ativação de restaurantes em massa")
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Não foi possivel encontrar um restaurante com o id passado"),
+		@ApiResponse(code = 400, message = "Id do restaurante é invalido")
+	})
 	public void ativarRestaurantes(List<Long> restaurantes);
 	
 	@ApiOperation("desativação de restaurantes em massa")
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Não foi possivel encontrar um restaurante com o id passado"),
+		@ApiResponse(code = 400, message = "Id do restaurante é invalido")
+	})
 	public void desativarRestaurantes(List<Long> restauranteids);
 	
 	@ApiOperation("Ativação de um unico restaurante")
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Não foi possivel encontrar um restaurante com o id passado"),
+		@ApiResponse(code = 400, message = "Id do restaurante é invalido")
+	})
 	public void ativarRestaurante(@ApiParam(value = "Id de um restaurante", example = "1", required = true) Long restauranteId);
 	
 	@ApiOperation("Desativação de um unico restaurante")
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Não foi possivel encontrar um restaurante com o id passado"),
+		@ApiResponse(code = 400, message = "Id do restaurante é invalido")
+	})
 	public void desativarRestaurante(@ApiParam(value = "Id de um restaurante", example = "1", required = true) Long restauranteId);
 	
 	@ApiOperation("Fechamento de um unico restaurante")
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Não foi possivel encontrar um restaurante com o id passado"),
+		@ApiResponse(code = 400, message = "Id do restaurante é invalido")
+	})
 	public void fecharRestaurante(@ApiParam(value = "Id de um restaurante", example = "1", required = true) Long restauranteId);
 	
 	@ApiOperation("Abertura de um unico restaurante")
+	@ApiResponses({
+		@ApiResponse(code = 404, message = "Não foi possivel encontrar um restaurante com o id passado"),
+		@ApiResponse(code = 400, message = "Id do restaurante é invalido")
+	})
 	public void abrirRestaurante(@ApiParam(value = "Id de um restaurante", example = "1", required = true) Long restauranteId);
 
 }
--- src/main/java/com/nesrux/jmfood/api/openapi/model/PedidosModelOpenApi.java
+++ src/main/java/com/nesrux/jmfood/api/openapi/model/PedidosModelOpenApi.java
@@ -1,30 +1,13 @@
 package com.nesrux.jmfood.api.openapi.model;
 
-import java.util.List;
-
 import com.nesrux.jmfood.api.model.dto.output.pedido.PedidoResumoModel;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
-
 @Getter
 @Setter
 @ApiModel("PedidosModel")
-public class PedidosModelOpenApi {
-	private List<PedidoResumoModel> content;
-
-	@ApiModelProperty(example = "10", value = "Quantidade de registros por página")
-	private Long size;
-
-	@ApiModelProperty(example = "50", value = "total de registros")
-	private Long totalElements;
-
-	@ApiModelProperty(example = "5", value = "Total de páginas")
-	private Long totalPages;
-
-	@ApiModelProperty(example = "0", value = "numero da página (começa em 0)")
-	private Long number;
+public class PedidosModelOpenApi extends PageModelOpenApi<PedidoResumoModel> {
 
 }
