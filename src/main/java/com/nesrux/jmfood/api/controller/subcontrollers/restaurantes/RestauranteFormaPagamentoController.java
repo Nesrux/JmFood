1d0101b2925f144fc1d2d39e67b8b14be403f831
Nesrux <joaomarcosdevs@gmail.com>
2023-06-28 16:05:16 -0300

    Desafio da DOC de restauranteFormaPagamentoControllerOpenApi completo

--- src/main/java/com/nesrux/jmfood/api/controller/subcontrollers/restaurantes/RestauranteFormaPagamentoController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/subcontrollers/restaurantes/RestauranteFormaPagamentoController.java
@@ -15,12 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nesrux.jmfood.api.classconversion.assembler.FormaPagamentoModelAssembler;
 import com.nesrux.jmfood.api.model.dto.output.formaPagamento.FormaPagamentoModel;
+import com.nesrux.jmfood.api.openapi.controller.restaurante.RestauranteFormaPagamentoControllerOpenApi;
 import com.nesrux.jmfood.domain.model.restaurante.Restaurante;
 import com.nesrux.jmfood.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping(path = "/restaurantes/{restauranteId}/formas-pagamento", produces = MediaType.APPLICATION_JSON_VALUE)
-public class RestauranteFormaPagamentoController {
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenApi {
 
 	@Autowired
 	private CadastroRestauranteService service;
