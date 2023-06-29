2062d570e279253c7b1534bdd3a67000735c4b0a
Nesrux <joaomarcosdevs@gmail.com>
2023-06-29 20:00:41 -0300

    desafio user controller feito com sucesso

--- src/main/java/com/nesrux/jmfood/api/openapi/controller/usuarios/UsuarioControllerOpenApi.java
+++ src/main/java/com/nesrux/jmfood/api/openapi/controller/usuarios/UsuarioControllerOpenApi.java
@@ -7,16 +7,53 @@ import com.nesrux.jmfood.api.model.dto.input.usuario.UsuarioInput;
 import com.nesrux.jmfood.api.model.dto.input.usuario.UsuarioInputAtualizar;
 import com.nesrux.jmfood.api.model.dto.output.usuario.UsuarioModel;
 
-public interface UsuarioControllerOpenApi {
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
+@Api(tags = "Usuario")
+public interface UsuarioControllerOpenApi {
+	
+	@ApiOperation("Listagem de usuarios")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Listagem feita com sucesso")
+	})
 	public List<UsuarioModel> listar();
 
-	public UsuarioModel buscar(Long usuarioId);
+	@ApiOperation("Busca de um unico usuario")
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "Codigo de usuario inválido"),
+		@ApiResponse(code = 404, message = "Id de usuario não existe"),
+		@ApiResponse(code = 200, message = "Busca de usuario feito com sucesso")
+	})
+	public UsuarioModel buscar(@ApiParam(example = "1", required = true) Long usuarioId);
 
-	public UsuarioModel salvar(UsuarioInput userInput);
+	@ApiOperation("Cadastro de um usuario")
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "representação de usuario inválida"),
+		@ApiResponse(code = 200, message = "cadastro de usuario feito com sucesso")
+	})
+	public UsuarioModel salvar(
+			@ApiParam(name = "Corpo", value = "Representação de um usuario", required = true) UsuarioInput userInput);
 
-	public UsuarioModel atualizar(Long usuarioId, UsuarioInputAtualizar usuarioInput);
+	@ApiOperation("Atualização de um usuario")
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "Codigo de usuario  ou representação de usuario inválido"),
+		@ApiResponse(code = 404, message = "Id de usuario não existe"),
+		@ApiResponse(code = 200, message = "Atualização de usuario feita com sucesso")
+	})
+	public UsuarioModel atualizar(@ApiParam(example = "1", required = true) Long usuarioId,
+			@ApiParam(name = "Corpo", value = "Representação de uma atualização de um usuario", required = true) UsuarioInputAtualizar usuarioInput);
 
-	public void atualizarSenha(Long usuarioId, TrocarSenhaInput senhainput);
+	@ApiOperation("Atualização de senha de um usuario")
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "Codigo de usuario ou representação de senha inválido"),
+		@ApiResponse(code = 404, message = "Id de usuario não existe"),
+		@ApiResponse(code = 204, message = "Atualização de senha feita com sucesso")
+	})
+	public void atualizarSenha(@ApiParam(example = "1", required = true) Long usuarioId,
+			@ApiParam(name = "Corpo", value = "Representação de senhas do usuario", required = true) TrocarSenhaInput senhainput);
 
 }
