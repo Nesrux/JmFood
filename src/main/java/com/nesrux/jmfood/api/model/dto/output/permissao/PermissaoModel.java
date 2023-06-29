c8e1dcbc26414ebd7710a95ce0211b288dadf70e
Nesrux <joaomarcosdevs@gmail.com>
2023-06-29 18:42:29 -0300

    desafio do grupoPermissãoController feito com sucesso

--- src/main/java/com/nesrux/jmfood/api/model/dto/output/permissao/PermissaoModel.java
+++ src/main/java/com/nesrux/jmfood/api/model/dto/output/permissao/PermissaoModel.java
@@ -1,5 +1,6 @@
 package com.nesrux.jmfood.api.model.dto.output.permissao;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,12 @@ import lombok.Setter;
 @Setter
 public class PermissaoModel {
 
+	@ApiModelProperty(example = "1", position = 5)
 	private Long id;
+
+	@ApiModelProperty(example = "Administrativo", position = 10)
 	private String nome;
+
+	@ApiModelProperty(example = "acesso total ao sistema", position = 15)
 	private String descricao;
 }
--- src/main/java/com/nesrux/jmfood/api/openapi/controller/grupos/GrupoPermissaoControllerOpenapi.java
+++ src/main/java/com/nesrux/jmfood/api/openapi/controller/grupos/GrupoPermissaoControllerOpenapi.java
@@ -4,14 +4,53 @@ import java.util.List;
 
 import com.nesrux.jmfood.api.model.dto.output.permissao.PermissaoModel;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Grupos")
 public interface GrupoPermissaoControllerOpenapi {
+	
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "Codigo de grupo inválido"),
+		@ApiResponse(code = 404, message = "codigo de grupo não existe"),
+		@ApiResponse(code = 200, message = "Listagem feita com sucesso")
+	})	
+	@ApiOperation("Lista todas as permissoes de um grupo")
+	public List<PermissaoModel> ListarPermissoes(Long grupoId);
 
-	public List<PermissaoModel> ListarPermissoes( Long grupoId);
+	@ApiOperation("Busca uma permissão de um grupo")
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "Codigo de grupo ou de permissão estão inválidos"),
+		@ApiResponse(code = 404, message = "Codigo de grupo ou de permissão não existem"),
+		@ApiResponse(code = 200, message = "Busca feita com sucesso")
+	})
+	public PermissaoModel buscarPermissao(
+			@ApiParam(example = "1", value = "Id de um grupo", required = true) Long grupoId,
+			@ApiParam(example = "1", value = "Id de uma permissao", required = true) Long permissaoId);
 
-	public PermissaoModel buscarPermissao( Long grupoId,  Long permissaoId);
+	
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "Codigo de grupo ou de permissão estão inválidos"),
+		@ApiResponse(code = 404, message = "Codigo de grupo ou de permissão não existem"),
+		@ApiResponse(code = 204, message = "Desassociação feita com sucesso")
+	})
+	@ApiOperation("Desassocia uma permissão de um grupo")
+	public void dessassociarPermissao(
+			@ApiParam(example = "1", value = "Id de um grupo", required = true) Long grupoId,
+			@ApiParam(example = "1", value = "Id de uma permissao", required = true) Long permissaoId);
 
-	public void dessassociarPermissao( Long grupoId,  Long permissaoId);
-
-	public void associarPermissao( Long grupoId,  Long permissaoId);
+	
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "Codigo de grupo ou de permissão estão inválidos"),
+		@ApiResponse(code = 404, message = "Codigo de grupo ou de permissão não existem"),
+		@ApiResponse(code = 204, message = "Associação feita com sucesso")
+	})
+	@ApiOperation("Associa uma permissão a um grupo")
+	public void associarPermissao(
+			@ApiParam(example = "1", value = "Id de um grupo", required = true) Long grupoId,
+			@ApiParam(example = "1", value = "Id de uma permissao", required = true) Long permissaoId);
 
 }
