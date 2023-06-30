f064170db4fab02c40e657bbfdb48fc443438bbf
Nesrux <joaomarcosdevs@gmail.com>
2023-06-30 17:31:56 -0300

    refatorando a classe FluxoPedidoControllerOpenApi

--- src/main/java/com/nesrux/jmfood/api/openapi/controller/pedido/FluxoPedidoControllerOpenApi.java
+++ src/main/java/com/nesrux/jmfood/api/openapi/controller/pedido/FluxoPedidoControllerOpenApi.java
@@ -1,5 +1,7 @@
 package com.nesrux.jmfood.api.openapi.controller.pedido;
 
+import com.nesrux.jmfood.api.exceptionHandler.ErroApi;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -11,8 +13,8 @@ public interface FluxoPedidoControllerOpenApi {
 
 	@ApiOperation("Confirmação de um pedido")
 	@ApiResponses({
-		@ApiResponse(code = 400, message = "Id de pedido inválido"),
-		@ApiResponse(code =  404, message =  "Não foi possivel confirmar pedido, pois o pedido com esse código nao existe"),
+		@ApiResponse(code = 400, message = "Id de pedido inválido", response = ErroApi.class),
+		@ApiResponse(code =  404, message =  "Não foi possivel confirmar pedido, pois o pedido com esse código nao existe", response = ErroApi.class),
 		@ApiResponse(code = 204, message = "Confirmação de pedido feito com sucesso")
 	})
 	public void confirmarPedido(
@@ -20,18 +22,17 @@ public interface FluxoPedidoControllerOpenApi {
 
 	@ApiOperation("entrega de um pedido")
 	@ApiResponses({
-		@ApiResponse(code = 400, message = "Id de pedido inválido"),
-		@ApiResponse(code =  404, message =  "Não foi possivel entregar pedido, pois o pedido com esse código nao existe"),
+		@ApiResponse(code = 400, message = "Id de pedido inválido", response = ErroApi.class),
+		@ApiResponse(code =  404, message =  "Não foi possivel entregar pedido, pois o pedido com esse código nao existe", response = ErroApi.class),
 		@ApiResponse(code = 204, message = "Entrega de pedido confirmado com sucesso")
-
 	})
 	public void entregarPedido(
 			@ApiParam(value = "codigo de um pedido", example = "d178b637-a785-4768-a3cb-aa1ce5a8cdab", required = true) String codigoPedido);
 
 	@ApiOperation("Cancelamento de um pedido")
 	@ApiResponses({
-		@ApiResponse(code = 400, message = "Id de pedido inválido"),
-		@ApiResponse(code =  404, message =  "Não foi possivel cancelar pedido, pois o pedido com esse código nao existe"),
+		@ApiResponse(code = 400, message = "Id de pedido inválido", response = ErroApi.class),
+		@ApiResponse(code =  404, message =  "Não foi possivel cancelar pedido, pois o pedido com esse código nao existe", response = ErroApi.class),
 		@ApiResponse(code = 204, message = "Cancelamento de pedido feito com sucesso")
 	})
 	public void cancelarPedido(
