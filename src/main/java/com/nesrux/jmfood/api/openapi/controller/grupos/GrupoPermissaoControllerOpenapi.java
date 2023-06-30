558e4c86fe3a7befba7f6eeef369f09bbe681b1c
Nesrux <joaomarcosdevs@gmail.com>
2023-06-30 17:30:48 -0300

    refatorando a classe grupoPermissaoCntrollerOpenApi

--- src/main/java/com/nesrux/jmfood/api/openapi/controller/grupos/GrupoPermissaoControllerOpenapi.java
+++ src/main/java/com/nesrux/jmfood/api/openapi/controller/grupos/GrupoPermissaoControllerOpenapi.java
@@ -2,6 +2,7 @@ package com.nesrux.jmfood.api.openapi.controller.grupos;
 
 import java.util.List;
 
+import com.nesrux.jmfood.api.exceptionHandler.ErroApi;
 import com.nesrux.jmfood.api.model.dto.output.permissao.PermissaoModel;
 
 import io.swagger.annotations.Api;
@@ -14,8 +15,8 @@ import io.swagger.annotations.ApiResponses;
 public interface GrupoPermissaoControllerOpenapi {
 	
 	@ApiResponses({
-		@ApiResponse(code = 400, message = "Codigo de grupo inválido"),
-		@ApiResponse(code = 404, message = "codigo de grupo não existe"),
+		@ApiResponse(code = 400, message = "Codigo de grupo inválido", response = ErroApi.class),
+		@ApiResponse(code = 404, message = "codigo de grupo não existe", response = ErroApi.class),
 		@ApiResponse(code = 200, message = "Listagem feita com sucesso")
 	})	
 	@ApiOperation("Lista todas as permissoes de um grupo")
@@ -23,8 +24,8 @@ public interface GrupoPermissaoControllerOpenapi {
 
 	@ApiOperation("Busca uma permissão de um grupo")
 	@ApiResponses({
-		@ApiResponse(code = 400, message = "Codigo de grupo ou de permissão estão inválidos"),
-		@ApiResponse(code = 404, message = "Codigo de grupo ou de permissão não existem"),
+		@ApiResponse(code = 400, message = "Codigo de grupo ou de permissão estão inválidos", response = ErroApi.class),
+		@ApiResponse(code = 404, message = "Codigo de grupo ou de permissão não existem", response = ErroApi.class),
 		@ApiResponse(code = 200, message = "Busca feita com sucesso")
 	})
 	public PermissaoModel buscarPermissao(
@@ -33,8 +34,8 @@ public interface GrupoPermissaoControllerOpenapi {
 
 	
 	@ApiResponses({
-		@ApiResponse(code = 400, message = "Codigo de grupo ou de permissão estão inválidos"),
-		@ApiResponse(code = 404, message = "Codigo de grupo ou de permissão não existem"),
+		@ApiResponse(code = 400, message = "Codigo de grupo ou de permissão estão inválidos", response = ErroApi.class),
+		@ApiResponse(code = 404, message = "Codigo de grupo ou de permissão não existem", response = ErroApi.class),
 		@ApiResponse(code = 204, message = "Desassociação feita com sucesso")
 	})
 	@ApiOperation("Desassocia uma permissão de um grupo")
@@ -44,8 +45,8 @@ public interface GrupoPermissaoControllerOpenapi {
 
 	
 	@ApiResponses({
-		@ApiResponse(code = 400, message = "Codigo de grupo ou de permissão estão inválidos"),
-		@ApiResponse(code = 404, message = "Codigo de grupo ou de permissão não existem"),
+		@ApiResponse(code = 400, message = "Codigo de grupo ou de permissão estão inválidos", response = ErroApi.class),
+		@ApiResponse(code = 404, message = "Codigo de grupo ou de permissão não existem", response = ErroApi.class),
 		@ApiResponse(code = 204, message = "Associação feita com sucesso")
 	})
 	@ApiOperation("Associa uma permissão a um grupo")
