87e08749310f34acb9ef6a17d52d7c56c7b75bce
Nesrux <joaomarcosdevs@gmail.com>
2023-07-01 14:31:00 -0300

    teste

---
+++ src/main/java/com/nesrux/jmfood/api/utils/UriUtils.java
@@ -0,0 +1,26 @@
+package com.nesrux.jmfood.api.utils;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+public class UriUtils {
+	
+	public static void addUriResponseHandler(Object resourceId) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+				.path("/{id}")
+				.buildAndExpand(resourceId).toUri();
+		
+		HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+				.getResponse();
+
+		response.setHeader(HttpHeaders.LOCATION, uri.toString());
+
+	}
+
+}
