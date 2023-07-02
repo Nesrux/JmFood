57b4829c6dbc4ea71494b8b6bc03e8d69badd33d
Nesrux <joaomarcosdevs@gmail.com>
2023-07-02 12:55:54 -0300

    adicionando links dinamicos referenciando ao método em cidade controller

--- src/main/java/com/nesrux/jmfood/api/controller/CidadeController.java
+++ src/main/java/com/nesrux/jmfood/api/controller/CidadeController.java
@@ -1,11 +1,14 @@
 package com.nesrux.jmfood.api.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -51,17 +54,21 @@ public class CidadeController implements CidadeControllerOpenApi {
 
 		CidadeModel cidadeModel = cidadeAssembler.toModel(cidade);
 
-		cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class).slash(cidadeModel.getId()).withSelfRel());
+		Link link = linkTo(methodOn(CidadeController.class)
+				.buscar(cidadeModel.getId()))
+				.withSelfRel();
+		cidadeModel.add(link);
 
-		cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class).withRel("cidades"));
+		Link linkListagemCidade = linkTo(methodOn(CidadeController.class)
+				.listar())
+				.withRel("Cidades");
+		cidadeModel.add(linkListagemCidade);
 
-		cidadeModel.getEstado()
-				.add(WebMvcLinkBuilder.linkTo(EstadoController.class).slash(cidadeModel.getEstado().getId()).withSelfRel());
-
-		// cidadeModel.add(new Link("http://localhost:8080/cidades/1"));
-		// cidadeModel.add(new Link("http://localhost:8080/cidades/", "cidades"));
-		// cidadeModel.getEstado().add(new Link("http://localhost:8080/estados/1"));
-
+		Link linkBuscaEstado = linkTo(methodOn(EstadoController.class)
+				.buscar(cidadeModel.getEstado().getId()))
+				.withRel("Estado");
+		cidadeModel.getEstado().add(linkBuscaEstado);
+		
 		return cidadeModel;
 	}
 
