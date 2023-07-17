7b9fe8f012acbabec0f8a47ce18b157e45da91c6
Nesrux <joaomarcosdevs@gmail.com>
2023-07-17 18:50:05 -0300

    CONFIG: configurando usuarios em memória

--- src/main/java/com/nesrux/jmfood/core/security/WebSecurityConfig.java
+++ src/main/java/com/nesrux/jmfood/core/security/WebSecurityConfig.java
@@ -1,16 +1,20 @@
 package com.nesrux.jmfood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	
+	//Configuração do Basic http
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.httpBasic()
@@ -25,6 +29,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 		.csrf().disable();
 	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("joao").password(passEncoder().encode("123")).roles("ADMIN")
+			.and()
+			.withUser("jmfood123").password(passEncoder().encode("123")).roles("ANY");
+	}
+	@Bean
+	public PasswordEncoder passEncoder() {
+		return new BCryptPasswordEncoder();
+	}	
 	
 	
 }
