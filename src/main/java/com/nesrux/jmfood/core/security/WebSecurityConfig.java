c4749970949e84b2da933ea6bfe4a32d214deced
Nesrux <joaomarcosdevs@gmail.com>
2023-07-17 18:38:01 -0300

    CONFIG: configurando api Stateless e csrf

---
+++ src/main/java/com/nesrux/jmfood/core/security/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.nesrux.jmfood.core.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+		.and()
+		.authorizeRequests()
+			.antMatchers("/v1/cozinhas/**").permitAll()
+			.antMatchers("/v1/restaurantes/**").permitAll()
+			.anyRequest().authenticated()
+		.and()
+		.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.csrf().disable();
+	}
+	
+	
+}
