918735a37eb77cae280538ea51ed168af907f72e
Nesrux <joaomarcosdevs@gmail.com>
2023-07-24 18:38:56 -0300

    Config: configurando o resourceServer com token opaco

--- src/main/java/com/nesrux/jmfood/core/security/ResourceServeConfig.java
+++ src/main/java/com/nesrux/jmfood/core/security/ResourceServeConfig.java
@@ -1,14 +1,9 @@
 package com.nesrux.jmfood.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,30 +12,10 @@ public class ResourceServeConfig extends WebSecurityConfigurerAdapter {
 	//Configuração do Basic http
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic()
+		http
+			.authorizeRequests().anyRequest().authenticated()
 		.and()
-		.authorizeRequests()
-			.antMatchers("/v1/cozinhas/**").permitAll()
-			.antMatchers("/v1/restaurantes/**").permitAll()
-			.anyRequest().authenticated()
-		.and()
-		.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-		.csrf().disable();
+		.oauth2ResourceServer().opaqueToken();
 	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("joao").password(passEncoder().encode("123")).roles("ADMIN")
-			.and()
-			.withUser("jmfood123").password(passEncoder().encode("123")).roles("ANY");
-	}
-	@Bean
-	public PasswordEncoder passEncoder() {
-		return new BCryptPasswordEncoder();
-	}	
-	
 	
 }
