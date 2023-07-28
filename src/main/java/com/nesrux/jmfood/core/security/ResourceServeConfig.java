393fe1747058243504ca95364dc66377239f7f3d
Nesrux <joaomarcosdevs@gmail.com>
2023-07-28 15:03:20 -0300

    Config: configurando o Sha256 decrip o resource server

--- src/main/java/com/nesrux/jmfood/core/security/ResourceServeConfig.java
+++ src/main/java/com/nesrux/jmfood/core/security/ResourceServeConfig.java
@@ -1,9 +1,14 @@
 package com.nesrux.jmfood.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +22,13 @@ public class ResourceServeConfig extends WebSecurityConfigurerAdapter {
 		.and()
 			.cors()
 		.and()
-			.oauth2ResourceServer().opaqueToken();
+			.oauth2ResourceServer().jwt();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		var secretKey = new SecretKeySpec("jkaie3049kanc8alpeo029irydhznalep029alsk18s".getBytes(), "HmacSha256");
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 	
 }
