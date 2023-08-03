a29cbd60aab70a2fa4d62bcf76d6c4af0ec361a5
Nesrux <joaomarcosdevs@gmail.com>
2023-08-03 19:01:35 -0300

    Test: adicionando grupos aos usuarios restantes

--- src/main/resources/db/imports/afterMigrate.sql
+++ src/main/resources/db/imports/afterMigrate.sql
@@ -177,7 +177,7 @@ insert into usuario (id, nome, email, senha, data_cadastro) values
 (8, 'Carlos Lima', 'jmfoodmail@gmail.com', '$2y$12$NSsM4gEOR7MKogflKR7GMeYugkttjNhAJMvFdHrBLaLp2HzlggP5W', utc_timestamp);
 
 
-insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
+insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2), (3, 3), (4, 4);
 
 insert into restaurante_usuario_responsavel (restaurante_id, usuario_id) values (1, 5), (3, 5);
 
