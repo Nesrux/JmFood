c749a36544508c6be9df855c8fba8ada45802397
Nesrux <joaomarcosdevs@gmail.com>
2023-08-03 20:35:14 -0300

    Feat: carregando as authorities e restringindo os acessos aos endpoints

--- src/main/java/com/nesrux/jmfood/core/security/ResourceServeConfig.java
+++ src/main/java/com/nesrux/jmfood/core/security/ResourceServeConfig.java
@@ -1,9 +1,15 @@
 package com.nesrux.jmfood.core.security;
 
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -13,13 +19,34 @@ public class ResourceServeConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests().anyRequest().authenticated()
+			.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/v1/cozinhas/**").hasAnyAuthority("EDITAR_COZINHAS")
+				.antMatchers(HttpMethod.PUT, "/v1/cozinhas/**").hasAnyAuthority("EDITAR_COZINHAS")
+				.antMatchers(HttpMethod.GET, "/v1/cozinhas/**").authenticated()
+				
+			.anyRequest().denyAll()
 		.and()
 			.cors()
 		.and()
-			.oauth2ResourceServer().jwt();
+			.oauth2ResourceServer()
+			.jwt()
+			.jwtAuthenticationConverter(converter());
 	}
 	
+	private JwtAuthenticationConverter converter () {
+		var jwtAuth = new JwtAuthenticationConverter();
+		jwtAuth.setJwtGrantedAuthoritiesConverter(jwt ->{
+			var authorities = jwt.getClaimAsStringList("authorities");
+			if(authorities == null) {
+				authorities = Collections.emptyList();
+			}
+			
+			return authorities.stream()
+					.map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList());
+		});
+		return jwtAuth;
+	}
 
 	//jkaie3049kanc8alpeo029irydhznalep029alsk18s
 }
