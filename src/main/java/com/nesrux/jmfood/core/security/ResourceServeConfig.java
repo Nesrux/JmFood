1c2f08b829a20ddcc65ffbb6b3f2790f814b1168
Nesrux <joaomarcosdevs@gmail.com>
2023-08-04 14:31:24 -0300

    Reafactor: carregando os escopos e transformando eles em authorities

--- src/main/java/com/nesrux/jmfood/core/security/ResourceServeConfig.java
+++ src/main/java/com/nesrux/jmfood/core/security/ResourceServeConfig.java
@@ -1,5 +1,6 @@
 package com.nesrux.jmfood.core.security;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -35,9 +38,15 @@ public class ResourceServeConfig extends WebSecurityConfigurerAdapter {
 				authorities = Collections.emptyList();
 			}
 			
-			return authorities.stream()
+			var scopesAuthoritiesConverter =  new JwtGrantedAuthoritiesConverter();
+			Collection<GrantedAuthority> grantedAuthorities = scopesAuthoritiesConverter.convert(jwt);
+			
+			grantedAuthorities.addAll(authorities.stream()
 					.map(SimpleGrantedAuthority::new)
-					.collect(Collectors.toList());
+					.collect(Collectors.toList()));
+			
+			
+			return grantedAuthorities;
 		});
 		return jwtAuth;
 	}
