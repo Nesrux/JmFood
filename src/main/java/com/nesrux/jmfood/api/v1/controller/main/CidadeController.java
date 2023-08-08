963e9c96594cd89727f9c8b2d4f625146b123c1f
Nesrux <joaomarcosdevs@gmail.com>
2023-08-08 17:20:25 -0300

    Feat: restringindo acessos dos endpoints de /cidades

--- src/main/java/com/nesrux/jmfood/api/v1/controller/main/CidadeController.java
+++ src/main/java/com/nesrux/jmfood/api/v1/controller/main/CidadeController.java
@@ -23,6 +23,7 @@ import com.nesrux.jmfood.api.v1.classconversion.dissasembler.CidadeInputDisassem
 import com.nesrux.jmfood.api.v1.model.dto.input.cidade.CidadeInputDto;
 import com.nesrux.jmfood.api.v1.model.dto.output.cidade.CidadeModel;
 import com.nesrux.jmfood.api.v1.openapi.controller.cidades.CidadeControllerOpenApi;
+import com.nesrux.jmfood.core.security.anotations.CheckSecurity;
 import com.nesrux.jmfood.domain.exception.NegocioException;
 import com.nesrux.jmfood.domain.exception.negocioException.entidadeNaoEncontrada.EstadoNaoEncontradoException;
 import com.nesrux.jmfood.domain.model.endereco.Cidade;
@@ -41,6 +42,7 @@ public class CidadeController implements CidadeControllerOpenApi {
 
 	@Override
 	@GetMapping
+	@CheckSecurity.Cidades.podeConsultar
 	public CollectionModel<CidadeModel> listar() {
 		List<Cidade> cidadeList = cidadeService.acharTodas();
 		
@@ -50,6 +52,7 @@ public class CidadeController implements CidadeControllerOpenApi {
 	@Override
 	@GetMapping("/{cidadeId}")
 	@ResponseStatus(HttpStatus.OK)
+	@CheckSecurity.Cidades.podeConsultar
 	public CidadeModel buscar(@PathVariable Long cidadeId) {
 		Cidade cidade = cidadeService.acharOuFalhar(cidadeId);
 
@@ -61,6 +64,7 @@ public class CidadeController implements CidadeControllerOpenApi {
 	@Override
 	@PostMapping()
 	@ResponseStatus(HttpStatus.CREATED)
+	@CheckSecurity.Cidades.podeEditar
 	public CidadeModel adicionar(@RequestBody @Valid CidadeInputDto cidadeInputDto) {
 		try {
 			Cidade cidade = cidadeDisassembler.toDomainObject(cidadeInputDto);
@@ -74,6 +78,7 @@ public class CidadeController implements CidadeControllerOpenApi {
 
 	@Override
 	@PutMapping("/{cidadeId}")
+	@CheckSecurity.Cidades.podeEditar
 	public CidadeModel atualizar(@PathVariable Long cidadeId, @RequestBody @Valid CidadeInputDto cidadeInputDto) {
 		try {
 			Cidade cidadeAtual = cidadeService.acharOuFalhar(cidadeId);
@@ -90,6 +95,7 @@ public class CidadeController implements CidadeControllerOpenApi {
 	@Override
 	@DeleteMapping("{cidadeId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@CheckSecurity.Cidades.podeEditar
 	public void excluir(@PathVariable Long cidadeId) {
 		cidadeService.excluir(cidadeId);
 	}
